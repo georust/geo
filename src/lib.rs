1fdb1121f3d85ce4e7a011bac18e8354b43df2a9
Corey Farwell <coreyf@rwell.org>
2014-12-30 11:21:35 -0800

    Beginning of Wkt struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
 mod tokenizer;
 
+pub struct Wkt;
+
+impl Wkt {
+}
+
+fn from_reader(reader: &mut Reader) -> Wkt {
+    match reader.read_to_string() {
+        Ok(string) => from_string(string),
+        Err(err) => panic!(err),
+    }
+}
+
+fn from_string(string: String) -> Wkt {
+    Wkt
+}
+
 #[test]
 fn it_works() {
 }
