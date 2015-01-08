03621b979dab1bef00ee922b6dcbadcf04dba7fd
Corey Farwell <coreyf@rwell.org>
2015-01-07 22:49:26 -0500

    Associated types have been added to Rust snapshot

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(associated_types)]
-
 use std::ascii::AsciiExt;
 
 use tokenizer::{PeekableTokens, Token, Tokens};
