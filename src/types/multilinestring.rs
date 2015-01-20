024136da1a0d9a83806c882672c63d71c19324c6
Corey Farwell <coreyf@rwell.org>
2015-01-20 00:53:49 -0500

    Remove unused imports

--- src/types/multilinestring.rs
+++ src/types/multilinestring.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use tokenizer::{PeekableTokens, Token};
+use tokenizer::PeekableTokens;
 use types::FromTokens;
 use types::linestring::LineString;
 use WktItem;
