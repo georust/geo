83db0724d14452460eff34abb68b29b6441976f1
Corey Farwell <coreyf@rwell.org>
2015-02-11 21:24:05 -0500

    Remove unused core feature

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(collections,core,std_misc)]
+#![feature(collections,std_misc)]
 
 use std::ascii::AsciiExt;
 use std::default::Default;
