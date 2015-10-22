69f295b9fe33b7a4a4e2b71227f1c4da53708d07
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-10-22 14:09:07 +0200

    TravisCI: Replace ghp-import with shell commands
    Installing ghp-import apparently needs sudo, which means that the containerized
    infrastructure of TravisCI can't be used.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,14 @@
 set -e
 
 cargo doc
-echo '<meta http-equiv=refresh content=0;url=geo/index.html>' > target/doc/index.html
-sudo pip install ghp-import
-ghp-import -n target/doc
-git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+cd target/doc
+
+echo '<meta http-equiv=refresh content=0;url=geo/index.html>' > index.html
+touch .nojekyll
+
+git init
+git config user.name "Travis"
+git config user.email "noreply@travis-ci.org"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages > /dev/null 2>&1
