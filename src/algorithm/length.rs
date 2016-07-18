8a5db6ef8c9d9383d9635af6a737d63e2702297d
Zambelli Pietro <pietro.zambelli@eurac.edu>
2016-07-18 07:34:09 +0200

    Implement Length for MultiLineString

--- src/algorithm/length.rs
+++ src/algorithm/length.rs
@@ -1,6 +1,6 @@
 use num::{Float};
 
-use types::{LineString};
+use types::{LineString, MultiLineString};
 use algorithm::distance::Distance;
 
 /// Calculation of the length
@@ -40,9 +40,20 @@ impl<T> Length<T> for LineString<T>
     }
 }
 
+impl<T> Length<T> for MultiLineString<T>
+    where T: Float
+{
+    ///
+    /// Length of a MultiLineString.
+    ///
+    fn length(&self) -> T {
+        self.0.iter().fold(T::zero(), |total, line| total + line.length())
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use types::{Coordinate, Point, LineString};
+    use types::{Coordinate, Point, LineString, MultiLineString};
     use algorithm::length::Length;
 
     #[test]
@@ -60,5 +71,12 @@ mod test {
         let p = |x| Point(Coordinate { x: x, y: 1. });
         let linestring = LineString(vec![p(1.), p(7.), p(8.), p(9.), p(10.), p(11.)]);
         assert_eq!(10.0_f64, linestring.length());
+    }
+    #[test]
+    fn multilinestring_test() {
+        let p = |x, y| Point(Coordinate { x: x, y: y });
+        let mline = MultiLineString(vec![LineString(vec![p(1., 0.), p(7., 0.), p(8., 0.), p(9., 0.), p(10., 0.), p(11., 0.)]),
+                                         LineString(vec![p(0., 0.), p(0., 5.)])]);
+        assert_eq!(15.0_f64, mline.length());
     }
 }
