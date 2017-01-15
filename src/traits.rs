04c96e938d91dbd3258de6b8da32d45d4e7e0792
Corey Farwell <coreyf@rwell.org>
2017-01-15 10:26:58 -0500

    Add traits for each geometry type.
    https://github.com/georust/rust-geo/issues/67

--- src/traits.rs
+++ src/traits.rs
@@ -6,3 +6,49 @@ pub trait ToGeo<T: Float>
 {
     fn to_geo(&self) -> Geometry<T>;
 }
+
+pub trait Point {
+    fn x(&self) -> f64;
+    fn y(&self) -> f64;
+    fn opt_z(&self) -> Option<f64> {
+        None
+    }
+    fn opt_m(&self) -> Option<f64> {
+        None
+    }
+}
+
+pub trait LineString<'a> {
+    type ItemType: 'a + Point;
+    type Iter: Iterator<Item=&'a Self::ItemType>;
+
+    fn points(&'a self) -> Self::Iter;
+}
+
+pub trait Polygon<'a> {
+    type ItemType: 'a + LineString<'a>;
+    type Iter: Iterator<Item=&'a Self::ItemType>;
+
+    fn rings(&'a self) -> Self::Iter;
+}
+
+pub trait MultiPoint<'a> {
+    type ItemType: 'a + Point;
+    type Iter: Iterator<Item=&'a Self::ItemType>;
+
+    fn points(&'a self) -> Self::Iter;
+}
+
+pub trait MultiLineString<'a> {
+    type ItemType: 'a + LineString<'a>;
+    type Iter: Iterator<Item=&'a Self::ItemType>;
+
+    fn lines(&'a self) -> Self::Iter;
+}
+
+pub trait MultiPolygon<'a> {
+    type ItemType: 'a + Polygon<'a>;
+    type Iter: Iterator<Item=&'a Self::ItemType>;
+
+    fn polygons(&'a self) -> Self::Iter;
+}
