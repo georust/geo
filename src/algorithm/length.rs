c141a222547829b21fb26c48fc60e80df0580a01
mattbatt <matthew.battifarano@gmail.com>
2017-05-24 12:01:37 -0400

    Implement Length for Line

--- src/algorithm/length.rs
+++ src/algorithm/length.rs
@@ -1,6 +1,6 @@
 use num_traits::Float;
 
-use types::{LineString, MultiLineString};
+use types::{Line, LineString, MultiLineString};
 use algorithm::distance::Distance;
 
 /// Calculation of the length
@@ -23,6 +23,14 @@ pub trait Length<T, RHS = Self> {
     fn length(&self) -> T;
 }
 
+impl<T> Length<T> for Line<T>
+    where T: Float
+{
+    fn length(&self) -> T {
+        self.start.distance(&self.end)
+    }
+}
+
 impl<T> Length<T> for LineString<T>
     where T: Float
 {
@@ -42,7 +50,7 @@ impl<T> Length<T> for MultiLineString<T>
 
 #[cfg(test)]
 mod test {
-    use types::{Coordinate, Point, LineString, MultiLineString};
+    use types::{Coordinate, Point, Line, LineString, MultiLineString};
     use algorithm::length::Length;
 
     #[test]
@@ -67,5 +75,12 @@ mod test {
         let mline = MultiLineString(vec![LineString(vec![p(1., 0.), p(7., 0.), p(8., 0.), p(9., 0.), p(10., 0.), p(11., 0.)]),
                                          LineString(vec![p(0., 0.), p(0., 5.)])]);
         assert_eq!(15.0_f64, mline.length());
+    }
+    #[test]
+    fn line_test() {
+        let line0 = Line::new(Point::new(0., 0.), Point::new(0., 1.));
+        let line1 = Line::new(Point::new(0., 0.), Point::new(3., 4.));
+        assert_eq!(line0.length(), 1.);
+        assert_eq!(line1.length(), 5.);
     }
 }
