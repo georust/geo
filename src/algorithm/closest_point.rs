6000c68727beb1eab83b4c6252eb526c6ceac43d
Michael Bryan <michaelfbryan@gmail.com>
2017-09-21 20:40:50 +0800

    Implemented closest point for LineString

--- src/algorithm/closest_point.rs
+++ src/algorithm/closest_point.rs
@@ -1,6 +1,6 @@
 use num_traits::Float;
 use prelude::*;
-use {Line, Point};
+use {Line, LineString, Point};
 
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub enum Closest<F: Float> {
@@ -29,12 +29,13 @@ pub enum Closest<F: Float> {
 /// assert_eq!(closest, Closest::SinglePoint(Point::new(0.0, 0.0)));
 /// ```
 pub trait ClosestPoint<F: Float, Rhs = Point<F>> {
-    fn closest_point(&self, other: &Rhs) -> Closest<F>;
+    /// Find the closest point between `self` and `p`.
+    fn closest_point(&self, p: &Rhs) -> Closest<F>;
 }
 
 impl<F: Float> ClosestPoint<F> for Point<F> {
-    fn closest_point(&self, other: &Self) -> Closest<F> {
-        if self == other {
+    fn closest_point(&self, p: &Self) -> Closest<F> {
+        if self == p {
             Closest::Intersection(*self)
         } else {
             Closest::SinglePoint(*self)
@@ -44,7 +45,7 @@ impl<F: Float> ClosestPoint<F> for Point<F> {
 
 
 impl<F: Float> ClosestPoint<F> for Line<F> {
-    fn closest_point(&self, other: &Point<F>) -> Closest<F> {
+    fn closest_point(&self, p: &Point<F>) -> Closest<F> {
         let line_length = self.length();
         if line_length == F::zero() {
             // if we've got a zero length line, technically the entire line
@@ -53,12 +54,14 @@ impl<F: Float> ClosestPoint<F> for Line<F> {
         }
 
         // For some line AB, there is some point, C, which will be closest to
-        // P (`other`). The line AB will be perpendicular to CP.
+        // P. The line AB will be perpendicular to CP.
+        //
+        // Line equation: P = start + t * (end - start)
 
         let direction_vector = self.end - self.start;
-        let to_other = *other - self.start;
+        let to_p = *p - self.start;
 
-        let t = to_other.dot(&direction_vector) / direction_vector.dot(&direction_vector);
+        let t = to_p.dot(&direction_vector) / direction_vector.dot(&direction_vector);
 
         // check the cases where the closest point is "outside" the line
         if t < F::zero() {
@@ -71,11 +74,36 @@ impl<F: Float> ClosestPoint<F> for Line<F> {
         let displacement = Point::new(t * x, t * y);
         let c = self.start + displacement;
 
-        if self.contains(other) {
+        if self.contains(p) {
             Closest::Intersection(c)
         } else {
             Closest::SinglePoint(c)
         }
+    }
+}
+
+impl<F: Float> ClosestPoint<F> for LineString<F> {
+    fn closest_point(&self, p: &Point<F>) -> Closest<F> {
+        let mut single_points = Vec::new();
+
+        for line_segment in self.lines() {
+            let closest = line_segment.closest_point(p);
+            match closest {
+                Closest::Intersection(_) => return closest,
+                Closest::SinglePoint(close) => single_points.push(close),
+                _ => {},
+            }
+        }
+
+        single_points
+            .into_iter()
+            .min_by(|l, r| {
+                l.distance(p)
+                    .partial_cmp(&r.distance(p))
+                    .expect("Should never get NaN")
+            })
+            .map(|closest_point| Closest::SinglePoint(closest_point))
+            .unwrap_or(Closest::Indeterminate)
     }
 }
 
@@ -117,5 +145,46 @@ mod tests {
 
         let got = line.closest_point(&p);
         assert_eq!(got, should_be);
+    }
+
+    fn points_to_line<F, P>(points: &[P]) -> LineString<F>
+    where
+        F: Float,
+        P: Into<Point<F>> + Clone,
+    {
+        points.iter().map(|p| p.clone().into()).collect()
+    }
+
+    #[test]
+    fn line_string_with_single_element_behaves_like_line() {
+        let points = vec![(0.0, 0.0), (100.0, 100.0)];
+        let line_string = points_to_line(&points);
+        let line = Line::new(points[0].into(), points[1].into());
+
+        let some_random_points = vec![
+            (0.0, 0.0),
+            (100.0, 100.0),
+            (50.0, 50.0),
+            (1000.0, 1000.0),
+            (100.0, 0.0),
+        ];
+
+        for (i, random_point) in some_random_points.into_iter().enumerate() {
+            let p: Point<f32> = random_point.into();
+
+            let got_from_line = line.closest_point(&p);
+            let got_from_line_string = line_string.closest_point(&p);
+
+            assert_eq!(got_from_line, got_from_line_string, "{}: {:?}", i, p);
+        }
+    }
+
+    #[test]
+    fn empty_line_string_is_indeterminate() {
+        let ls: LineString<f32> = LineString(Vec::new());
+        let p = Point::new(0.0, 0.0);
+
+        let got = ls.closest_point(&p);
+        assert_eq!(got, Closest::Indeterminate);
     }
 }
