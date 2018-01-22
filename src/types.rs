a6047e47b15fc6171b51ab82a16be973a25e4135
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2018-01-22 14:03:29 +0000

    Add SpatialObject impl for Line
    This lets us use Line in RTree, skipping the conversion to SimpleEdge

--- src/types.rs
+++ src/types.rs
@@ -6,10 +6,13 @@ use std::ops::Sub;
 use std::fmt::Debug;
 
 use std::iter::{self, FromIterator, Iterator};
-
+use algorithm::boundingbox::BoundingBox;
+use algorithm::distance::Distance;
+use num_traits::{Float, ToPrimitive};
 use spade::SpadeNum;
 use spade::{PointN, TwoDimensional};
 use num_traits::{Float, Num, NumCast, ToPrimitive};
+use spade::{BoundingRect, PointN, SpatialObject, TwoDimensional};
 
 /// The type of an x or y value of a point/coordinate.
 ///
@@ -552,6 +555,30 @@ where
     }
 }
 
+impl<T> SpatialObject for Line<T>
+where
+    T: Float + SpadeNum + Debug,
+{
+    type Point = Point<T>;
+
+    fn mbr(&self) -> BoundingRect<Self::Point> {
+        let bbox = self.bbox();
+        BoundingRect::from_corners(
+            &Point::new(bbox.xmin, bbox.ymin),
+            &Point::new(bbox.xmax, bbox.ymax),
+        )
+    }
+
+    fn distance2(&self, point: &Self::Point) -> <Self::Point as PointN>::Scalar {
+        let d = self.distance(point);
+        if d == T::zero() {
+            d
+        } else {
+            d.powi(2)
+        }
+    }
+}
+
 /// An ordered collection of two or more [`Point`s](struct.Point.html), representing a path between locations
 ///
 /// Create a `LineString` by calling it directly:
@@ -940,6 +967,7 @@ impl<T: CoordinateType> Geometry<T> {
 
 #[cfg(test)]
 mod test {
+    use spade::primitives::SimpleEdge;
     use types::*;
 
     #[test]
@@ -1008,5 +1036,18 @@ mod test {
 
         let p: Point<i64> = Point::new(1_000_000, 0);
         assert_eq!(p.x(), 1_000_000i64);
+    }
+
+    #[test]
+    /// ensure Line's SpatialObject impl is correct
+    fn line_test() {
+        let se = SimpleEdge::new(Point::new(0.0, 0.0), Point::new(5.0, 5.0));
+        let l = Line::new(Point::new(0.0, 0.0), Point::new(5.0, 5.0));
+        assert_eq!(se.mbr(), l.mbr());
+        // difference in 15th decimal place
+        assert_relative_eq!(
+            se.distance2(&Point::new(4.0, 10.0)),
+            l.distance2(&Point::new(4.0, 10.0))
+        );
     }
 }
