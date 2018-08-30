21e863a8d13f775dd852222959d4d1234d2a9008
Corey Farwell <coreyf@rwell.org>
2018-08-30 08:04:23 -0500

    remove duplicate FromTokens definition

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::default::Default;
-
-use tokenizer::{PeekableTokens, Token};
-
 pub use self::coord::Coord;
 pub use self::geometrycollection::GeometryCollection;
 pub use self::linestring::LineString;
@@ -33,46 +29,3 @@ mod multipoint;
 mod multipolygon;
 mod point;
 mod polygon;
-
-trait FromTokens: Sized + Default {
-    fn from_tokens(tokens: &mut PeekableTokens) -> Result<Self, &'static str>;
-
-    fn from_tokens_with_parens(tokens: &mut PeekableTokens) -> Result<Self, &'static str> {
-        match tokens.next() {
-            Some(Token::ParenOpen) => (),
-            Some(Token::Word(ref s)) if s.to_ascii_uppercase() == "EMPTY" => {
-                return Ok(Default::default())
-            }
-            _ => return Err("Missing open parenthesis for type"),
-        };
-        let result = FromTokens::from_tokens(tokens);
-        match tokens.next() {
-            Some(Token::ParenClose) => (),
-            _ => return Err("Missing closing parenthesis for type"),
-        };
-        result
-    }
-
-    fn comma_many<F>(f: F, tokens: &mut PeekableTokens) -> Result<Vec<Self>, &'static str>
-    where
-        F: Fn(&mut PeekableTokens) -> Result<Self, &'static str>,
-    {
-        let mut items = Vec::new();
-
-        match f(tokens) {
-            Ok(i) => items.push(i),
-            Err(s) => return Err(s),
-        };
-
-        while let Some(&Token::Comma) = tokens.peek() {
-            tokens.next(); // throw away comma
-
-            match f(tokens) {
-                Ok(i) => items.push(i),
-                Err(s) => return Err(s),
-            };
-        }
-
-        Ok(items)
-    }
-}
