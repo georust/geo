9e63ae05adebf81ccc0ab3e4f742fc4c944cd6a5
Jacob Zuo <chopinsky@live.com>
2019-05-18 14:23:43 -0500

    More support of the geo map type

--- geo-types/src/geo_map.rs
+++ geo-types/src/geo_map.rs
@@ -1,7 +1,11 @@
-use crate::{CoordinateType, Coordinate, Line, LineString, MultiLineString};
+use crate::{
+    CoordinateType, Coordinate, Line, LineString, MultiLineString, Point,
+};
 use std::collections::HashMap;
 use std::convert::From;
 use std::hash::Hash;
+
+const DEFAULT_EDGE_COUNT: usize = 4;
 
 #[derive(PartialEq, Clone, Copy, Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -13,10 +17,16 @@ pub enum MapDistanceType {
 
 #[derive(Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-struct EdgeInfo<T>
+struct NodeInfo<T>
 where
     T: CoordinateType + Eq + Hash
 {
+    // the node location
+    loc: Coordinate<T>,
+
+    // the name for the string
+    name: String,
+
     // vector of neighbor nodes that we can go to, stored in the form of (Destination, Distance)
     to: Vec<(Coordinate<T>, Option<T>)>,
 }
@@ -27,51 +37,70 @@ pub struct GeoMap<T: CoordinateType>
 where
     T: CoordinateType + Eq + Hash
 {
-    inner: HashMap<Coordinate<T>, EdgeInfo<T>>,
+    inner: HashMap<Coordinate<T>, NodeInfo<T>>,
     distance_type: MapDistanceType,
 }
 
 impl<T> GeoMap<T>
 where
-    T: CoordinateType + Eq + Hash,
+    T: CoordinateType + Eq + Hash
 {
     pub fn new<C>(src: C) -> Self
     where
-        C: Iterator<Item=Line<T>>
+        C: Iterator<Item = Line<T>>
     {
         GeoMap::build(src, None)
     }
 
     pub fn new_with_size<C>(src: C, size: usize) -> Self
-        where
-            C: Iterator<Item=Line<T>>
+    where
+        C: Iterator<Item = Line<T>>
     {
         GeoMap::build(src, Some(size))
     }
 
     fn build<C>(src: C, cap: Option<usize>) -> Self
-        where
-            C: Iterator<Item=Line<T>>
+    where
+        C: Iterator<Item = Line<T>>
     {
-        let mut inner: HashMap<Coordinate<T>, EdgeInfo<T>> =
+        // prepare the base store
+        let mut inner: HashMap<Coordinate<T>, NodeInfo<T>> =
             if let Some(size) = cap {
                 HashMap::with_capacity(size)
             } else {
                 HashMap::new()
             };
 
+        // update the node -> edge map
         src
             .for_each(|line| {
-                if let Some(edge) = inner.get_mut(&line.start) {
-                    edge.to.push((line.end, None));
+                if let Some(node) = inner.get_mut(&line.start) {
+                    node.to.push((line.end, None));
                     return;
                 }
 
-                let mut to = Vec::new();
+                // default to 4 edges, on average
+                let mut to = Vec::with_capacity(DEFAULT_EDGE_COUNT);
                 to.push((line.end, None));
-                inner.insert(line.start, EdgeInfo{ to });
+
+                // add the new node to the list
+                inner.insert(
+                    line.start,
+                    NodeInfo {
+                        loc: line.start,
+                        name: String::new(),
+                        to,
+                    }
+                );
             });
 
+        if cap.is_some() {
+            // if given a size estimation, it may be larger than what we actually need, shrink to
+            // save spaces
+            inner.shrink_to_fit();
+        }
+
+        // the edges may have duplicates, removing them now
         inner
             .iter_mut()
             .for_each(|val| {
@@ -84,6 +113,17 @@ where
             inner,
             distance_type: MapDistanceType::NotCalculated,
         }
+    }
+}
+
+impl<T> Iterator for GeoMap<T>
+where
+    T: CoordinateType + Eq + Hash
+{
+    type Item = Point<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.keys().map(|node| Point(node.to_owned())).next()
     }
 }
 
@@ -107,6 +147,12 @@ where
         let size = item.0.len();
         assert!(size > 0);
 
+        GeoMap::new_with_size(
+            item.lines(),
+            size - 1
+        )
+
+/*
         let mut it = item.0.into_iter();
         let mut start = it.next().unwrap();
 
@@ -118,6 +164,7 @@ where
             }),
             size - 1
         )
+*/
     }
 }
 
@@ -127,12 +174,15 @@ where
 {
     fn from(item: Vec<LineString<T>>) -> Self {
         assert!(item.len() > 0 && item[0].0.len() > 0);
-        let estimated_size = item.len() * item[0].0.len();
+
+        let estimated_size =
+            item.iter().fold(0, |acc, x| acc + x.0.len() -1);
 
         GeoMap::new_with_size(
-            item.into_iter().flat_map(|lines| {
-                assert!(lines.0.len() > 0);
-
+            item.iter().flat_map(|l| {
+                assert!(l.0.len() > 0);
+                l.lines()
+/*
                 let mut it = lines.0.into_iter();
                 let mut start = it.next().unwrap();
 
@@ -141,6 +191,7 @@ where
                     start = p;
                     return line;
                 })
+                */
             }),
             estimated_size
         )
