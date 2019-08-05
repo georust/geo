bffd17a44b48c813c2877447b74d977139df9d63
Corey Farwell <coreyf@rwell.org>
2019-08-04 22:51:22 -0400

    rustfmt

--- geo/src/traits.rs
+++ geo/src/traits.rs
@@ -2,7 +2,9 @@ pub use crate::Geometry;
 
 use crate::CoordinateType;
 
-#[deprecated(note = "Will be removed in an upcoming version. Switch to std::convert::Into<Geo> or std::convert::TryInto<Geo>.")]
+#[deprecated(
+    note = "Will be removed in an upcoming version. Switch to std::convert::Into<Geo> or std::convert::TryInto<Geo>."
+)]
 pub trait ToGeo<T: CoordinateType> {
     fn to_geo(&self) -> Geometry<T>;
 }
