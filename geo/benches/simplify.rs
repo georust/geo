bf57bf4800fe38b6e62a7a1e46a204d2cc5a3369
Stephan Hügel <shugel@tcd.ie>
2019-12-13 11:51:54 +0000

    Add f64 Visvalingam-Whyatt benchmark

--- geo/benches/simplify.rs
+++ geo/benches/simplify.rs
@@ -16,6 +16,14 @@ fn criterion_benchmark(c: &mut Criterion) {
         });
     });
 
+    c.bench_function("simplify vw simple f64", |bencher| {
+        let points = include!("../src/algorithm/test_fixtures/norway_main.rs");
+        let ls: LineString<f64> = points.into();
+        bencher.iter(|| {
+            let _ = ls.simplifyvw(&0.0005);
+        });
+    });
+
     c.bench_function("simplify vwp f32", |bencher| {
         let points = include!("../src/algorithm/test_fixtures/norway_main.rs");
         let ls: LineString<f32> = points.into();
