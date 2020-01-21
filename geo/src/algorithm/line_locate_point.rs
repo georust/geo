908c0e610a4deb62e671b70836ff654bc8bb4e5c
Tom Watson <tom.m.watson@gmail.com>
2020-01-21 23:19:08 +1100

    introudce line_locate_point and line_interpolate_point
    These functions mimic postgis functionality for tracing along lines
    and either computing a point along a line, or the fraction along
    the line where a closest point lies.
    
    wip line_interpolate_point, implemented for Line
    
    wip line_interpolate_point - test for line, impl for linestring
    
    line interpolate point
    
    tests and docs

---
+++ geo/src/algorithm/line_locate_point.rs
@@ -0,0 +1,162 @@
+use std::{
+    ops::AddAssign,
+};
+use crate::{
+    Point,
+    Line,
+    LineString,
+    CoordinateType,
+    algorithm::{
+        euclidean_distance::EuclideanDistance, 
+        euclidean_length::EuclideanLength
+    },
+};
+use num_traits::{
+    Float,
+    identities::{Zero, One},
+};
+
+/// Returns a fraction of the line's total length
+/// representing the location 
+/// of the closest point on the line to the given point.
+///
+/// # Examples
+///
+/// ```
+/// use geo::{LineString, point};
+/// use geo::algorithm::line_locate_point::LineLocatePoint;
+///
+/// let linestring: LineString<f64> = vec![
+///     [-1.0, 0.0],
+///     [0.0, 0.0],
+///     [0.0, 1.0]
+/// ].into();
+///
+/// assert_eq!(linestring.line_locate_point(&point!(x: -1.0, y: 0.0)), 0.0);
+/// assert_eq!(linestring.line_locate_point(&point!(x: -0.5, y: 0.0)), 0.25);
+/// assert_eq!(linestring.line_locate_point(&point!(x: 0.0, y: 0.0)), 0.5);
+/// assert_eq!(linestring.line_locate_point(&point!(x: 0.0, y: 0.5)), 0.75);
+/// assert_eq!(linestring.line_locate_point(&point!(x: 0.0, y: 1.0)), 1.0);
+/// ```
+pub trait LineLocatePoint<T, Rhs>
+{
+    type Output;
+    type Rhs;
+
+    fn line_locate_point(&self, p: &Rhs) -> Self::Output;
+}
+
+impl<T> LineLocatePoint<T, Point<T>> for Line<T> 
+where 
+    T: CoordinateType + Float + Zero + One
+{
+    type Output = T;
+    type Rhs = Point<T>;
+
+    fn line_locate_point(&self, p: &Self::Rhs) -> Self::Output {
+        let sp = [p.x() - self.start.x, p.y() - self.start.y];
+        let v = [self.end.x - self.start.x, self.end.y - self.start.y];
+        let v_sq = v[0] * v[0] + v[1] * v[1];
+        if v_sq == T::zero() { // The line has zero length, return zero
+            return T::zero();
+        } else {
+            let v_dot_sp = v[0] * sp[0] + v[1] * sp[1];
+            let l = v_dot_sp / v_sq;
+            if v_dot_sp.is_nan() | v_sq.is_nan() {
+                return T::nan();
+            } else {
+                return l.max(T::zero()).min(T::one());
+            }
+        }
+    }
+}
+
+impl<T> LineLocatePoint<T, Point<T>> for LineString<T>
+where
+    T: CoordinateType + Float + Zero + One + PartialOrd + AddAssign,
+    Line<T>: EuclideanDistance<T, Point<T>> + EuclideanLength<T>
+{
+    type Output = T;
+    type Rhs = Point<T>;
+
+    fn line_locate_point(&self, p: &Self::Rhs) -> Self::Output {
+        let mut total_length = T::zero();
+        let mut queue = Vec::new();
+        for line in self.lines() {
+            let length = line.euclidean_length();
+            let distance_to_point = line.euclidean_distance(p);
+            queue.push(
+                (
+                    total_length.clone(), 
+                    length.clone(), 
+                    distance_to_point.clone(), 
+                    line.clone()
+                )
+            );
+            total_length += length;
+        }
+        let l = queue.iter()
+            .min_by(|x, y| (x.2).partial_cmp(&y.2).unwrap())
+            .unwrap();
+
+        (l.0 + l.1 * (l.3).line_locate_point(p)) / total_length
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{
+        Coordinate, 
+        point
+    };
+
+    #[test]
+    fn test_line_locate_point_line() {
+        let line = Line::new(
+            Coordinate {x: -1.0, y: 0.0 },
+            Coordinate {x: 1.0, y: 0.0 }
+        );
+        let point = Point::new(0.0, 1.0);
+        assert_eq!(line.line_locate_point(&point), 0.5);
+
+        let point = Point::new(1.0, 1.0);
+        assert_eq!(line.line_locate_point(&point), 1.0);
+
+        let point = Point::new(2.0, 1.0);
+        assert_eq!(line.line_locate_point(&point), 1.0);
+
+        let point = Point::new(-1.0, 1.0);
+        assert_eq!(line.line_locate_point(&point), 0.0);
+
+        let point = Point::new(-2.0, 1.0);
+        assert_eq!(line.line_locate_point(&point), 0.0);
+
+        let line = Line::new(
+            Coordinate {x: 0.0, y: 0.0},
+            Coordinate {x: Float::infinity(), y:0.0}
+        );
+        let point = Point::new(1000.0, 1000.0);
+        assert_eq!(line.line_locate_point(&point), 0.0);
+
+        let line = Line::new(
+            Coordinate {x: 0.0, y: 0.0},
+            Coordinate {x: Float::nan(), y:0.0}
+        );
+        let point = Point::new(1000.0, 1000.0);
+        assert!(line.line_locate_point(&point).is_nan());
+    }
+
+    #[test]
+    fn test_line_locate_point_linestring() {
+        let ring: LineString<f64> = include!("./test_fixtures/ring.rs").into();
+        let pt = point!(x: 10.0, y: 1.0);
+        assert_eq!(ring.line_locate_point(&pt), 0.0);
+
+        let pt = point!(x: 10.0, y: 1.0000000000000742);
+        assert_eq!(ring.line_locate_point(&pt), 0.9999999999999988);
+
+        let pt = point!(x: 10.0, y: 1.0);
+        assert_eq!(ring.line_locate_point(&pt), 0.0);
+    }
+}
