f6972a198d2bee1c4a453700c8f80ef218834559
Corey Farwell <coreyf@rwell.org>
2020-05-09 16:14:12 -0400

    Use smaller polygon for simplify benchmarks.

--- geo/benches/simplify.rs
+++ geo/benches/simplify.rs
@@ -9,7 +9,7 @@ use geo::LineString;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("simplify vw simple f32", |bencher| {
-        let points = include!("../src/algorithm/test_fixtures/norway_main.rs");
+        let points = include!("../src/algorithm/test_fixtures/louisiana.rs");
         let ls: LineString<f32> = points.into();
         bencher.iter(|| {
             let _ = ls.simplifyvw(&0.0005);
@@ -17,7 +17,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("simplify vw simple f64", |bencher| {
-        let points = include!("../src/algorithm/test_fixtures/norway_main.rs");
+        let points = include!("../src/algorithm/test_fixtures/louisiana.rs");
         let ls: LineString<f64> = points.into();
         bencher.iter(|| {
             let _ = ls.simplifyvw(&0.0005);
@@ -25,7 +25,15 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("simplify vwp f32", |bencher| {
-        let points = include!("../src/algorithm/test_fixtures/norway_main.rs");
+        let points = include!("../src/algorithm/test_fixtures/louisiana.rs");
+        let ls: LineString<f32> = points.into();
+        bencher.iter(|| {
+            let _ = ls.simplifyvw_preserve(&0.0005);
+        });
+    });
+
+    c.bench_function("simplify vwp f64", |bencher| {
+        let points = include!("../src/algorithm/test_fixtures/louisiana.rs");
         let ls: LineString<f32> = points.into();
         bencher.iter(|| {
             let _ = ls.simplifyvw_preserve(&0.0005);
