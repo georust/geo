946ae0c6b3a5ba497d95e88d8d6271e7a5f9d0dd
Corey Farwell <coreyf@rwell.org>
2020-11-10 07:48:30 -0500

    add arbitrary implementations for all types in geo-types

--- geo-types/src/lib.rs
+++ geo-types/src/lib.rs
@@ -100,18 +100,111 @@ impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Ar
 }
 
 impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for Point<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        u.arbitrary::<Coordinate<T>>().map(Point)
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
     for LineString<T>
 {
     fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
-        let coords = u
-            .arbitrary_iter::<Coordinate<T>>()?
-            .collect::<Result<Vec<Coordinate<T>>, _>>()?;
+        let coords = u.arbitrary::<Vec<Coordinate<T>>>()?;
 
         if coords.len() < 2 {
             return Err(arbitrary::Error::IncorrectFormat);
         }
 
         Ok(LineString(coords))
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for Polygon<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        Ok(Polygon::new(
+            u.arbitrary::<LineString<T>>()?,
+            u.arbitrary::<Vec<LineString<T>>>()?
+        ))
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for MultiPoint<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        u.arbitrary::<Vec<Point<T>>>().map(MultiPoint)
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for MultiLineString<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        u.arbitrary::<Vec<LineString<T>>>().map(MultiLineString)
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for MultiPolygon<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        u.arbitrary::<Vec<Polygon<T>>>().map(MultiPolygon)
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for GeometryCollection<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        u.arbitrary()
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for Rect<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        Ok(Rect::new(
+            u.arbitrary::<Coordinate<T>>()?,
+            u.arbitrary::<Coordinate<T>>()?,
+        ))
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for Triangle<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        Ok(Triangle(
+            u.arbitrary::<Coordinate<T>>()?,
+            u.arbitrary::<Coordinate<T>>()?,
+            u.arbitrary::<Coordinate<T>>()?
+        ))
+    }
+}
+
+impl<T: num_traits::Float + arbitrary::Arbitrary + CoordinateType> arbitrary::Arbitrary
+    for Geometry<T>
+{
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        let n = u.int_in_range(0..=8)?;
+
+        Ok(match n {
+            0 => Geometry::Point(u.arbitrary()?),
+            1 => Geometry::LineString(u.arbitrary()?),
+            2 => Geometry::Polygon(u.arbitrary()?),
+            3 => Geometry::MultiPoint(u.arbitrary()?),
+            4 => Geometry::MultiLineString(u.arbitrary()?),
+            5 => Geometry::MultiPoint(u.arbitrary()?),
+            6 => Geometry::GeometryCollection(u.arbitrary()?),
+            7 => Geometry::Triangle(u.arbitrary()?),
+            8 => Geometry::Rect(u.arbitrary()?),
+            _ => unreachable!(),
+        })
     }
 }
 
