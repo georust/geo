35fc651cc246fa5d1bb11a0feb0597e0d32aeb22
Corey Farwell <coreyf@rwell.org>
2020-11-16 12:18:43 -0500

    remove another box

--- geo/src/algorithm/coords_iter.rs
+++ geo/src/algorithm/coords_iter.rs
@@ -53,48 +53,17 @@ impl<'a, T: CoordinateType + 'a> CoordsIter<'a, T> for LineString<T> {
 impl<'a, T: CoordinateType + 'a> CoordsIter<'a, T> for Polygon<T> {
     type Iter = iter::Chain<
         <geo_types::LineString<T> as CoordsIter<'a, T>>::Iter,
-        InteriorCoordsIter<'a, T>,
+        LineStringsToCoordsIter<'a, T>,
     >;
 
     fn coords_iter(&'a self) -> Self::Iter {
         self.exterior()
             .coords_iter()
             .chain(
-                InteriorCoordsIter(
-                    LineStringsToCoordsIter(self.interiors().iter()).flatten()
+                LineStringsToCoordsIter(
+                    LineStringsToCoordSliceIter(self.interiors().iter()).flatten()
                 )
             )
-    }
-}
-
-// Utility to transform Iterator<LineString> into Iterator<&[Coordinate]>
-struct LineStringsToCoordsIter<'a, T: CoordinateType>(slice::Iter<'a, LineString<T>>);
-
-impl<'a, T: CoordinateType> Iterator for LineStringsToCoordsIter<'a, T> {
-    type Item = &'a [Coordinate<T>];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|n| &n.0[..])
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.0.size_hint()
-    }
-}
-
-// Utility to transform Iterator<&[Coordinate]> into Iterator<Coordinate>
-#[doc(hidden)]
-pub struct InteriorCoordsIter<'a, T: CoordinateType>(iter::Flatten<LineStringsToCoordsIter<'a, T>>);
-
-impl<'a, T: CoordinateType> Iterator for InteriorCoordsIter<'a, T> {
-    type Item = Coordinate<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().copied()
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.0.size_hint()
     }
 }
 
@@ -103,26 +72,10 @@ impl<'a, T: CoordinateType> Iterator for InteriorCoordsIter<'a, T> {
 // └───────────────────────────────┘
 
 impl<'a, T: CoordinateType + 'a> CoordsIter<'a, T> for MultiPoint<T> {
-    type Iter = MultiPointCoordsIter<'a, T>;
+    type Iter = PointsToCoordsIter<'a, T>;
 
     fn coords_iter(&'a self) -> Self::Iter {
-        MultiPointCoordsIter(self.0.iter())
-    }
-}
-
-// Utility to transform Iterator<Point> into Iterator<Coordinate>
-#[doc(hidden)]
-pub struct MultiPointCoordsIter<'a, T: CoordinateType>(slice::Iter<'a, Point<T>>);
-
-impl<'a, T: CoordinateType> Iterator for MultiPointCoordsIter<'a, T> {
-    type Item = Coordinate<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|point| point.0)
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.0.size_hint()
+        PointsToCoordsIter(self.0.iter())
     }
 }
 
@@ -131,10 +84,12 @@ impl<'a, T: CoordinateType> Iterator for MultiPointCoordsIter<'a, T> {
 // └────────────────────────────────────┘
 
 impl<'a, T: CoordinateType + 'a> CoordsIter<'a, T> for MultiLineString<T> {
-    type Iter = Box<dyn Iterator<Item = Coordinate<T>> + 'a>;
+    type Iter = LineStringsToCoordsIter<'a, T>;
 
     fn coords_iter(&'a self) -> Self::Iter {
-        Box::new(self.0.iter().flat_map(|m| m.coords_iter()))
+        LineStringsToCoordsIter(
+            LineStringsToCoordSliceIter(self.0.iter()).flatten()
+        )
     }
 }
 
@@ -221,11 +176,62 @@ impl<'a, T: CoordinateType + 'a> CoordsIter<'a, T> for Geometry<T> {
             Geometry::LineString(g) => Box::new(g.coords_iter()),
             Geometry::Polygon(g) => Box::new(g.coords_iter()),
             Geometry::MultiPoint(g) => Box::new(g.coords_iter()),
-            Geometry::MultiLineString(g) => g.coords_iter(),
+            Geometry::MultiLineString(g) => Box::new(g.coords_iter()),
             Geometry::MultiPolygon(g) => g.coords_iter(),
             Geometry::GeometryCollection(g) => g.coords_iter(),
             Geometry::Rect(g) => g.coords_iter(),
             Geometry::Triangle(g) => g.coords_iter(),
         }
+    }
+}
+
+// ┌───────────┐
+// │ Utilities │
+// └───────────┘
+
+// Utility to transform Iterator<Point> into Iterator<Coordinate>
+#[doc(hidden)]
+pub struct PointsToCoordsIter<'a, T: CoordinateType>(slice::Iter<'a, Point<T>>);
+
+impl<'a, T: CoordinateType> Iterator for PointsToCoordsIter<'a, T> {
+    type Item = Coordinate<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|point| point.0)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+// Utility to transform Iterator<LineString> into Iterator<&[Coordinate]>
+struct LineStringsToCoordSliceIter<'a, T: CoordinateType>(slice::Iter<'a, LineString<T>>);
+
+impl<'a, T: CoordinateType> Iterator for LineStringsToCoordSliceIter<'a, T> {
+    type Item = &'a [Coordinate<T>];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|n| &n.0[..])
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+// Utility to transform Iterator<&[Coordinate]> into Iterator<Coordinate>
+#[doc(hidden)]
+pub struct LineStringsToCoordsIter<'a, T: CoordinateType>(iter::Flatten<LineStringsToCoordSliceIter<'a, T>>);
+
+impl<'a, T: CoordinateType> Iterator for LineStringsToCoordsIter<'a, T> {
+    type Item = Coordinate<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().copied()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
     }
 }
