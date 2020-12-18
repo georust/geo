9de3748819dad3730de0f6b1bd733c4eb0338fd4
Corey Farwell <coreyf@rwell.org>
2020-12-18 09:28:21 -0500

    better boundingrect test

--- geo/fuzz/fuzz_targets/bounding_rect.rs
+++ geo/fuzz/fuzz_targets/bounding_rect.rs
@@ -1,8 +1,18 @@
 #![no_main]
 
 use geo::algorithm::bounding_rect::BoundingRect;
+use geo::algorithm::contains::Contains;
+use geo::algorithm::intersects::Intersects;
+use geo::algorithm::coords_iter::CoordsIter;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|geometry: geo::Geometry<f32>| {
-    geometry.bounding_rect();
+    let rect = match geometry.bounding_rect() {
+        Some(r) => r,
+        None => return,
+    };
+
+    for coord in geometry.coords_iter() {
+        assert!(rect.contains(&coord) || rect.intersects(&coord));
+    }
 });
