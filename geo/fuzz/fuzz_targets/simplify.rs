1a1942e0721d27033734bd1c87d73f93b0152f00
Corey Farwell <coreyf@rwell.org>
2021-02-26 12:33:25 -0500

    Remove unnecessary negative epsilon check

--- geo/fuzz/fuzz_targets/simplify.rs
+++ geo/fuzz/fuzz_targets/simplify.rs
@@ -4,8 +4,5 @@ use geo::algorithm::simplify::Simplify;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|tuple: (geo_types::Polygon<f32>, f32)| {
-    if tuple.1 < 0. {
-        return;
-    }
     tuple.0.simplify(&tuple.1);
 });
