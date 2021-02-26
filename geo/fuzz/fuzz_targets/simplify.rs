1e36d1e07bd7b83b6132940c66ebedb6204c3c8e
Corey Farwell <coreyf@rwell.org>
2021-02-26 09:18:09 -0500

    Add first fuzz target for fuzzing simplify algorithm.

---
+++ geo/fuzz/fuzz_targets/simplify.rs
@@ -0,0 +1,11 @@
+#![no_main]
+
+use geo::algorithm::simplify::Simplify;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|tuple: (geo_types::Polygon<f32>, f32)| {
+    if tuple.1 < 0. {
+        return;
+    }
+    tuple.0.simplify(&tuple.1);
+});
