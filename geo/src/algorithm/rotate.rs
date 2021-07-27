6a73c6509b0808b81f3c6f3baaa9cf5ebdc1932e
Alex Poulos <alex@equul.us>
2021-07-27 14:03:41 -0400

    adding a debug_assert for empty linestring

--- geo/src/algorithm/rotate.rs
+++ geo/src/algorithm/rotate.rs
@@ -164,7 +164,16 @@ where
     fn rotate(&self, angle: T) -> Self {
         match self.centroid() {
             Some(centroid) => rotate_many(angle, centroid, self.points_iter()).collect(),
-            None => self.clone(),
+            None => {
+                // LineString was empty and had no computable centroid
+                let points = self.clone().into_points();
+                debug_assert!(
+                    points.len() == 0,
+                    "Expected `None` centroid to only occur for an empty LineString, but this LineString has {} points", 
+                    points.len()
+                );
+                self.clone()
+            }
         }
     }
 }
