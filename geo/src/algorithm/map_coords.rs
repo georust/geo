93307dc91618253b8c90af31cb8b9f52e174e44a
Michael Kirk <michael.code@endoftheworl.de>
2022-05-16 17:57:54 -0700

    partial revert of c0a720af where explicit f64 was good documentation

--- geo/src/algorithm/map_coords.rs
+++ geo/src/algorithm/map_coords.rs
@@ -62,9 +62,6 @@ mod modern {
         /// # use approx::assert_relative_eq;
         ///
         /// let SCALE_FACTOR: f64 = 1000000.0;
-        /// let p1: Point<f32> = Point::new(10.0f32, 20.0f32);
-        /// let p2: Point<f64> = p1.map_coords(|Coordinate { x, y }| (x as f64, y as f64).into());
-        ///
         /// let floating_point_geom: Point<f64> = Point::new(10.15f64, 20.05f64);
         /// let fixed_point_geom: Point<i32> = floating_point_geom.map_coords(|Coordinate { x, y }| {
         ///     Coordinate { x: (x * SCALE_FACTOR) as i32, y: (y * SCALE_FACTOR) as i32 }
