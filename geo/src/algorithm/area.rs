30d6c9ccfa17869adfeb80e5f1c5050ea24a868d
Michael Kirk <michael.code@endoftheworl.de>
2022-06-02 13:47:30 -0700

    Map coords takes a coordinate

--- geo/src/algorithm/area.rs
+++ geo/src/algorithm/area.rs
@@ -34,7 +34,7 @@ where
     let mut tmp = T::zero();
     for line in linestring.lines() {
         use crate::MapCoords;
-        let line = line.map_coords(|(x, y)| (x - shift.x, y - shift.y));
+        let line = line.map_coords(|c| c - shift);
         tmp = tmp + line.determinant();
     }
 
@@ -313,11 +313,10 @@ mod test {
 
         let area = polygon.signed_area();
 
-        let shift_x = 1.5e8;
-        let shift_y = 1.5e8;
+        let shift = coord! { x: 1.5e8, y: 1.5e8 };
 
         use crate::map_coords::MapCoords;
-        let polygon = polygon.map_coords(|(x, y)| (x + shift_x, y + shift_y));
+        let polygon = polygon.map_coords(|c| c + shift);
 
         let new_area = polygon.signed_area();
         let err = (area - new_area).abs() / area;
