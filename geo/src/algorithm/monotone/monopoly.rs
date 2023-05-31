d72342811e10e99f86cc483209bca4a5b28462c3
Rajsekar Manokaran <rajsekar@gmail.com>
2023-05-31 21:52:33 +0530

    Add CoordPosition impl for MonoPoly

--- geo/src/algorithm/monotone/monopoly.rs
+++ geo/src/algorithm/monotone/monopoly.rs
@@ -1,9 +1,15 @@
-use crate::{BoundingRect, GeoNum, LineString, Polygon, Rect, sweep::SweepPoint};
+use geo_types::Line;
+
+use crate::{
+    coordinate_position::CoordPos, sweep::SweepPoint, BoundingRect, Coord, CoordinatePosition,
+    GeoNum, HasKernel, Kernel, LineString, Orientation, Polygon, Rect,
+};
 
 /// Monotone polygon
-/// 
+///
 /// A monotone polygon is a polygon that can be decomposed into two
-/// monotone chains.
+/// monotone chains (along the X-axis). This implies any vertical line
+/// intersects the polygon at most twice (or not at all).
 pub struct MonoPoly<T: GeoNum> {
     top: LineString<T>,
     bot: LineString<T>,
@@ -45,17 +51,16 @@ impl<T: GeoNum> std::fmt::Debug for MonoPoly<T> {
 }
 
 impl<T: GeoNum> MonoPoly<T> {
+    /// Create a monotone polygon from the top and bottom chains.
+    ///
+    /// Note: each chain must be a strictly increasing sequence (in the lexigraphic order),
+    /// with the same start and end points.  Further, the top chain must be strictly above
+    /// the bottom chain except at the end-points.
     pub(super) fn new(top: LineString<T>, bot: LineString<T>) -> Self {
+        // TODO: move these to debug-only asserts
         assert_eq!(top.0.first(), bot.0.first());
         assert_eq!(top.0.last(), bot.0.last());
         assert_ne!(top.0.first(), top.0.last());
-
-        for win in top.0.windows(2).chain(bot.0.windows(2)) {
-            if SweepPoint::from(win[0]) >= SweepPoint::from(win[1]) {
-                eprintln!("ERR: {:?} >= {:?}", win[0], win[1]);
-            }
-            assert!(SweepPoint::from(win[0]) < SweepPoint::from(win[1]));
-        }
         Self { top, bot }
     }
 
@@ -71,8 +76,56 @@ impl<T: GeoNum> MonoPoly<T> {
         &self.bot
     }
 
+    /// Convert self to (top, bottom) pair of chains.
     pub fn into_ls_pair(self) -> (LineString<T>, LineString<T>) {
         (self.top, self.bot)
+    }
+
+    /// Get the pair of segments in the chain that intersect the Y-axis at the given x-coordinate.
+    pub fn bounding_segment(&self, x: T) -> Option<(Line<T>, Line<T>)> {
+        // binary search for the segment that contains the x coordinate.
+
+        let tl_idx = match self
+            .top
+            .0
+            .binary_search_by(|coord| coord.x.partial_cmp(&x).unwrap())
+        {
+            Ok(idx) => {
+                if idx == self.top.0.len() - 1 {
+                    idx - 1
+                } else {
+                    idx
+                }
+            }
+            Err(idx) => {
+                if idx == 0 || idx == self.top.0.len() {
+                    return None;
+                } else {
+                    idx - 1
+                }
+            }
+        };
+        let bl_idx = match self
+            .bot
+            .0
+            .binary_search_by(|coord| coord.x.partial_cmp(&x).unwrap())
+        {
+            Ok(idx) => {
+                if idx == self.bot.0.len() - 1 {
+                    idx - 1
+                } else {
+                    idx
+                }
+            }
+            Err(idx) => {
+                debug_assert!(idx > 0 && idx < self.bot.0.len());
+                idx - 1
+            }
+        };
+        Some((
+            Line::new(self.top.0[tl_idx], self.top.0[tl_idx + 1]),
+            Line::new(self.bot.0[bl_idx], self.bot.0[bl_idx + 1]),
+        ))
     }
 
     /// Convert self into a [`Polygon`].
@@ -91,4 +144,39 @@ impl<T: GeoNum> MonoPoly<T> {
     }
 }
 
+impl<T: GeoNum> CoordinatePosition for MonoPoly<T> {
+    type Scalar = T;
 
+    fn calculate_coordinate_position(
+        &self,
+        coord: &Coord<Self::Scalar>,
+        is_inside: &mut bool,
+        boundary_count: &mut usize,
+    ) {
+        let (top, bot) = if let Some(t) = self.bounding_segment(coord.x) {
+            t
+        } else {
+            return;
+        };
+
+        match <T as HasKernel>::Ker::orient2d(top.start, *coord, top.end) {
+            Orientation::Clockwise => return,
+            Orientation::Collinear => {
+                *is_inside = true;
+                *boundary_count += 1;
+                return;
+            }
+            _ => {}
+        }
+        match <T as HasKernel>::Ker::orient2d(bot.start, *coord, bot.end) {
+            Orientation::CounterClockwise => return,
+            Orientation::Collinear => {
+                *is_inside = true;
+                *boundary_count += 1;
+            }
+            _ => {
+                *is_inside = true;
+            }
+        }
+    }
+}
