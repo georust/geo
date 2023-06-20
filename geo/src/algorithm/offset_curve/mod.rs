850c50d5cb697876ee3023994cfc17c72e25b6b9
thehappycheese <the.nicholas.archer@gmail.com>
2023-06-20 23:47:40 +0800

    remove old offset_curve approach

--- geo/src/algorithm/offset_curve/mod.rs
+++ geo/src/algorithm/offset_curve/mod.rs
@@ -1,14 +1,9 @@
 
 mod vector_extensions;
-mod slice_itertools;
 mod line_intersection;
 mod offset_segments_iterator;
 mod offset_line_raw;
 mod line_measured;
-
-// Kept temporarily during transition to new approach
-mod offset_curve_trait_old;
-use offset_curve_trait_old::OffsetCurveOld;
 
 mod offset_curve_trait;
 pub use offset_curve_trait::OffsetCurve;
