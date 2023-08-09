520191c2e9f6e93b6175a9438fba183d0be778f6
thehappycheese <the.nicholas.archer@gmail.com>
2023-08-09 23:22:29 +0800

    added into_tuple for result types

--- geo/src/algorithm/line_split.rs
+++ geo/src/algorithm/line_split.rs
@@ -1,27 +1,150 @@
-use crate::{CoordFloat, EuclideanLength, Line, LineString};
+//!
 
+
+use crate::{CoordFloat, EuclideanLength, Line, LineString};
 
 
 #[rustfmt::skip]
 #[derive(PartialEq, Debug)]
-pub enum LineSplitResult<Result> {
-    First            (Result        ),
-    Second           (        Result),
-    FirstSecond      (Result, Result),
-    InvalidLineString,
+pub enum LineSplitResult<T> {
+    First       (T   ),
+    Second      (   T),
+    FirstSecond (T, T),
+}
+
+
+#[rustfmt::skip]
+impl<T> LineSplitResult<T>{
+    pub fn first(&self) -> Option<&T> {
+        match self {
+            Self::First      (x   ) => Some(x),
+            Self::Second     (   _) => None,
+            Self::FirstSecond(x, _) => Some(x),
+        }
+    }
+    pub fn into_first(self) -> Option<T> {
+        match self {
+            Self::First      (x   ) => Some(x),
+            Self::Second     (   _) => None,
+            Self::FirstSecond(x, _) => Some(x),
+        }
+    }
+    pub fn second(&self) -> Option<&T> {
+        match self {
+            Self::First      (_   ) => None,
+            Self::Second     (   x) => Some(x),
+            Self::FirstSecond(_, x) => Some(x),
+        }
+    }
+    pub fn into_second(self) -> Option<T> {
+        match self {
+            Self::First      (_   ) => None,
+            Self::Second     (   x) => Some(x),
+            Self::FirstSecond(_, x) => Some(x),
+        }
+    }
+
+    pub fn into_tuple(self) -> (Option<T>, Option<T>) {
+        match self {
+            Self::First      (a   ) => (Some(a), None   ),
+            Self::Second     (   b) => (None   , Some(b)),
+            Self::FirstSecond(a, b) => (Some(a), Some(b)),
+        }
+    }
 }
 
 #[rustfmt::skip]
 #[derive(PartialEq, Debug)]
-pub enum LineSplitTwiceResult<Result> {
-    First            (Result                ),
-    Second           (        Result        ),
-    Third            (                Result),
-    FirstSecond      (Result, Result        ),
-    SecondThird      (        Result, Result),
-    FirstThird       (Result,         Result),
-    FirstSecondThird (Result, Result, Result),
-    InvalidLineString
+pub enum LineSplitTwiceResult<T> {
+    First            (T      ),
+    Second           (   T   ),
+    Third            (      T),
+    FirstSecond      (T, T   ),
+    SecondThird      (   T, T),
+    FirstThird       (T,    T),
+    FirstSecondThird (T, T, T),
+}
+
+
+#[rustfmt::skip]
+impl<T> LineSplitTwiceResult<T> {
+    pub fn first(&self) -> Option<&T> {
+        match self {
+            Self::First           (x      ) => Some(x),
+            Self::Second          (   _   ) => None,
+            Self::Third           (      _) => None,
+            Self::FirstSecond     (x, _   ) => Some(x),
+            Self::SecondThird     (   _, _) => None,
+            Self::FirstThird      (x,    _) => Some(x),
+            Self::FirstSecondThird(x, _, _) => Some(x),
+        }
+    }
+    pub fn into_first(self) -> Option<T> {
+        match self {
+            Self::First           (x      ) => Some(x),
+            Self::Second          (   _   ) => None,
+            Self::Third           (      _) => None,
+            Self::FirstSecond     (x, _   ) => Some(x),
+            Self::SecondThird     (   _, _) => None,
+            Self::FirstThird      (x,    _) => Some(x),
+            Self::FirstSecondThird(x, _, _) => Some(x),
+        }
+    }
+    pub fn second(&self) -> Option<&T> {
+        match self {
+            Self::First           (_      ) => None,
+            Self::Second          (   x   ) => Some(x),
+            Self::Third           (      _) => None,
+            Self::FirstSecond     (_, x   ) => Some(x),
+            Self::SecondThird     (   x, _) => Some(x),
+            Self::FirstThird      (_,    _) => None,
+            Self::FirstSecondThird(_, x, _) => Some(x),
+        }
+    }
+    pub fn into_second(self) -> Option<T> {
+        match self {
+            Self::First           (_      ) => None,
+            Self::Second          (   x   ) => Some(x),
+            Self::Third           (      _) => None,
+            Self::FirstSecond     (_, x   ) => Some(x),
+            Self::SecondThird     (   x, _) => Some(x),
+            Self::FirstThird      (_,    _) => None,
+            Self::FirstSecondThird(_, x, _) => Some(x),
+        }
+    }
+    pub fn third(&self) -> Option<&T> {
+        match self {
+            Self::First           (_      ) => None,
+            Self::Second          (   x   ) => Some(x),
+            Self::Third           (      _) => None,
+            Self::FirstSecond     (_, x   ) => Some(x),
+            Self::SecondThird     (   x, _) => Some(x),
+            Self::FirstThird      (_,    _) => None,
+            Self::FirstSecondThird(_, x, _) => Some(x),
+        }
+    }
+    pub fn into_third(self) -> Option<T> {
+        match self {
+            Self::First           (_      ) => None,
+            Self::Second          (   _   ) => None,
+            Self::Third           (      x) => Some(x),
+            Self::FirstSecond     (_, _   ) => None,
+            Self::SecondThird     (   _, x) => Some(x),
+            Self::FirstThird      (_,    x) => Some(x),
+            Self::FirstSecondThird(_, _, x) => Some(x),
+        }
+    }
+    pub fn into_tuple(self) -> (Option<T>, Option<T>, Option<T>) {
+        match self {
+            Self::First           (a      ) => (Some(a), None   , None   ),
+            Self::Second          (   b   ) => (None   , Some(b), None   ),
+            Self::Third           (      c) => (None   , None   , Some(c)),
+            Self::FirstSecond     (a, b   ) => (Some(a), Some(b), None   ),
+            Self::SecondThird     (   b, c) => (None   , Some(b), Some(c)),
+            Self::FirstThird      (a,    c) => (Some(a), None   , Some(c)),
+            Self::FirstSecondThird(a, b, c) => (Some(a), Some(b), Some(c)),
+        }
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -62,8 +185,28 @@ fn measure_line_string<Scalar>(line_string:&LineString<Scalar>) -> Option<LineSt
 ///
 pub trait LineSplit<Scalar> where Self:Sized, Scalar: CoordFloat {
 
-    fn line_split(&self, fraction: Scalar) -> LineSplitResult<Self>;
+    /// Note on choice of return type:
+    /// 
+    /// You may wonder why this does not return `Option<(Option<Line>, Option<Line>)>`?
+    /// It is because then the return type causes uncertainty; The user may expect to possibly
+    /// receive `Some((None, None))` which is never possible, this would lead to clutter in match
+    /// statements.
+    /// 
+    /// To make it easier to 'just get the first' or 'just get the second' you can use
+    /// `LineSplitResult::first()` and `LineSplitResult::second()` which return `Option<T>`
+    /// 
+    /// 
+    fn line_split(&self, fraction: Scalar) -> Option<LineSplitResult<Self>>;
 
+    /// Note on choice of return type:
+    /// 
+    /// You may wonder why this does not return `Option<(Option<Line>,Option<Line>,Option<Line>)>`?
+    /// It is because then the return type causes uncertainty; The user may expect to possibly
+    /// receive `Some((None, None, None))` which is never possible.
+    /// The user would have a hard time writing an exhaustive match statement.
+    /// 
+    /// To make it easier to 'just get the second' the `LineSplitResult` has a function called `first()->Option<T>`
+    /// 
     // TODO: I only want to skip formatting the match block, but because attributes on expressions
     //       are experimental we are forced to put it on the function to avoid an error message.
     #[rustfmt::skip]
@@ -71,7 +214,7 @@ pub trait LineSplit<Scalar> where Self:Sized, Scalar: CoordFloat {
         &self,
         start_fraction: Scalar,
         end_fraction: Scalar,
-    ) -> LineSplitTwiceResult<Self> {
+    ) -> Option<LineSplitTwiceResult<Self>> {
         // import enum variants
         use LineSplitTwiceResult::*;
 
@@ -86,36 +229,39 @@ pub trait LineSplit<Scalar> where Self:Sized, Scalar: CoordFloat {
         let second_fraction = (end_fraction - start_fraction) / (Scalar::one() - start_fraction);
 
         match self.line_split(start_fraction) {
-            LineSplitResult::FirstSecond(line1, line2) => match line2.line_split(second_fraction) {
-                LineSplitResult::FirstSecond(line2, line3) => FirstSecondThird(line1, line2, line3),
-                LineSplitResult::First      (line2       ) => FirstSecond     (line1, line2       ),
-                LineSplitResult::Second     (       line3) => FirstThird      (line1,        line3),
-                LineSplitResult::InvalidLineString => InvalidLineString,
+            Some(LineSplitResult::FirstSecond(line1, line2)) => match line2.line_split(second_fraction) {
+                Some(LineSplitResult::FirstSecond(line2, line3)) => Some(FirstSecondThird(line1, line2, line3)),
+                Some(LineSplitResult::First      (line2       )) => Some(FirstSecond     (line1, line2       )),
+                Some(LineSplitResult::Second     (       line3)) => Some(FirstThird      (line1,        line3)),
+                None => None,
             },
-            LineSplitResult::First (line1) => First(line1),
-            LineSplitResult::Second(line2) => match line2.line_split(second_fraction) {
-                LineSplitResult::FirstSecond(line2, line3) => SecondThird     (       line2, line3), 
-                LineSplitResult::First      (line2       ) => Second          (       line2       ),
-                LineSplitResult::Second     (       line3) => Third           (              line3),
-                LineSplitResult::InvalidLineString => InvalidLineString,
+            Some(LineSplitResult::First (line1)) => Some(First(line1)),
+            Some(LineSplitResult::Second(line2)) => match line2.line_split(second_fraction) {
+                Some(LineSplitResult::FirstSecond(line2, line3)) => Some(SecondThird     (       line2, line3)), 
+                Some(LineSplitResult::First      (line2       )) => Some(Second          (       line2       )),
+                Some(LineSplitResult::Second     (       line3)) => Some(Third           (              line3)),
+                None => None,
             },
-            LineSplitResult::InvalidLineString => InvalidLineString,
+            None => None,
         }
     }
 }
 
 impl<Scalar> LineSplit<Scalar> for Line<Scalar> where Scalar: CoordFloat {
-    fn line_split(&self, fraction: Scalar) -> LineSplitResult<Self> {
+    fn line_split(&self, fraction: Scalar) -> Option<LineSplitResult<Self>> {
+        if fraction.is_nan() {
+            return None
+        }
         if fraction <= Scalar::zero() {
-            LineSplitResult::Second(self.clone())
+            Some(LineSplitResult::Second(self.clone()))
         } else if fraction >= Scalar::one() {
-            LineSplitResult::First(self.clone())
+            Some(LineSplitResult::First(self.clone()))
         } else {
             let new_midpoint = self.start + self.delta() * fraction;
-            LineSplitResult::FirstSecond(
+            Some(LineSplitResult::FirstSecond(
                 Line::new(self.start, new_midpoint),
                 Line::new(new_midpoint, self.end),
-            )
+            ))
         }
     }
 }
@@ -124,14 +270,16 @@ impl<Scalar> LineSplit<Scalar> for LineString<Scalar>
 where
     Scalar: CoordFloat + std::iter::Sum,
 {
-    fn line_split(&self, fraction: Scalar) -> LineSplitResult<Self> {
+    fn line_split(&self, fraction: Scalar) -> Option<LineSplitResult<Self>> {
         // import enum variants
         use LineSplitResult::*;
-        match (fraction.is_finite(), fraction <= Scalar::zero(), fraction >= Scalar::one()){
-            (false, _, _) => InvalidLineString,
-            (true, true, false) => First(self.clone()),
-            (true, false, true) => Second(self.clone()),
-            (true, _, _) => {
+        if fraction.is_nan(){
+            return None
+        }
+        match (fraction <= Scalar::zero(), fraction >= Scalar::one()){
+            (false, true) => Some(First(self.clone())),
+            (true, false) => Some(Second(self.clone())),
+            _ => {
                 // find the total length, and at the same time the length of each segment
                 // TODO: consider the possibility of a `LineStringMeasured` datatype in the future
                 //       as this will be a common requirement in several algorithms, and would be a big
@@ -139,11 +287,11 @@ where
                 //       I think I saw a PreparedGeometry PR? maybe that will cover this?
                 let LineStringMeasurements{length_total, length_segments} = match measure_line_string(&self) {
                     Some(x) =>x,
-                    None=> return InvalidLineString
+                    None=> return None
                 };
                 if ! Scalar::is_finite(length_total) || Scalar::is_zero(&length_total) {
                     // TODO: Does this cover a linestring with zero or one points?
-                    return InvalidLineString
+                    return None
                 }
 
                 // Find the length of the first part of the line string before the split;
@@ -171,24 +319,24 @@ where
                         // TODO: check for divide by zero
                         let fraction_to_split_segment = (length_fraction - length_accumulated_before_segment) / length_segment;
                         match Line::new(a, b).line_split(fraction_to_split_segment) {
-                            FirstSecond(line1, _line2) => {
+                            Some(FirstSecond(line1, _line2)) => {
                                 coords_first_part.push(line1.end);
                                 coords_second_part.push(line1.end);
                                 coords_second_part.push(b);
                             },
-                            First      (_line1       ) => {
+                            Some(First      (_line1       )) => {
                                 coords_first_part.push(b);
                                 coords_second_part.push(b);
                             },
-                            Second     (       _line2) => {
+                            Some(Second     (       _line2)) => {
                                 coords_second_part.push(a);
                                 coords_second_part.push(b);
                             },
-                            InvalidLineString => return InvalidLineString
+                            None => return None
                         }
                     }
                 }
-                FirstSecond(coords_first_part.into(), coords_second_part.into())
+                Some(FirstSecond(coords_first_part.into(), coords_second_part.into()))
             }
         }
     }
@@ -266,7 +414,7 @@ mod test {
             coord!{x:10.0_f32, y:0.0_f32},
         );
         let result = line.line_split(0.6);
-        assert_eq!(result, LineSplitResult::FirstSecond(
+        assert_eq!(result, Some(LineSplitResult::FirstSecond(
             Line::new(
                 coord!{x: 0.0_f32, y:0.0_f32},
                 coord!{x: 6.0_f32, y:0.0_f32},
@@ -275,7 +423,7 @@ mod test {
                 coord!{x: 6.0_f32, y:0.0_f32},
                 coord!{x:10.0_f32, y:0.0_f32},
             )
-        ));
+        )));
 
         // simple y-axis aligned check
         let line = Line::new(
@@ -283,7 +431,7 @@ mod test {
             coord!{x:0.0_f32, y:10.0_f32},
         );
         let result = line.line_split(0.3);
-        assert_eq!(result, LineSplitResult::FirstSecond(
+        assert_eq!(result, Some(LineSplitResult::FirstSecond(
             Line::new(
                 coord!{x:0.0_f32, y:0.0_f32},
                 coord!{x:0.0_f32, y:3.0_f32},
@@ -292,7 +440,7 @@ mod test {
                 coord!{x:0.0_f32, y:3.0_f32},
                 coord!{x:0.0_f32, y:10.0_f32},
             )
-        ));
+        )));
 
         // non_trivial check
         let line = Line::new(
@@ -301,7 +449,7 @@ mod test {
         );
         let split_point = line.start + line.delta() * 0.7;
         let result = line.line_split(0.7);
-        assert_eq!(result, LineSplitResult::FirstSecond(
+        assert_eq!(result, Some(LineSplitResult::FirstSecond(
             Line::new(
                 line.start,
                 split_point,
@@ -310,7 +458,7 @@ mod test {
                 split_point,
                 line.end,
             )
-        ));
+        )));
     }
 
     #[test]
@@ -321,7 +469,7 @@ mod test {
             coord!{x:10.0_f32, y:0.0_f32},
         );
         let result = line.line_split(1.0);
-        assert_eq!(result, LineSplitResult::First(line));
+        assert_eq!(result, Some(LineSplitResult::First(line)));
 
         // Test numbers larger than one
         let line = Line::new(
@@ -329,7 +477,7 @@ mod test {
             coord!{x:10.0_f32, y:0.0_f32},
         );
         let result = line.line_split(2.0);
-        assert_eq!(result, LineSplitResult::First(line));
+        assert_eq!(result, Some(LineSplitResult::First(line)));
     }
     #[test]
     fn test_measure_line_split_second() {
@@ -339,7 +487,7 @@ mod test {
             coord!{x:10.0_f32, y:0.0_f32},
         );
         let result = line.line_split(0.0);
-        assert_eq!(result, LineSplitResult::Second(line));
+        assert_eq!(result, Some(LineSplitResult::Second(line)));
 
         // Test negative numbers
         let line = Line::new(
@@ -347,8 +495,52 @@ mod test {
             coord!{x:10.0_f32, y:0.0_f32},
         );
         let result = line.line_split(-2.0);
-        assert_eq!(result, LineSplitResult::Second(line));
+        assert_eq!(result, Some(LineSplitResult::Second(line)));
     }
+
+
+    #[test]
+    fn test_measure_linestring_split() {
+        let line_string:LineString<f32> = line_string![
+            (x:0.0, y:0.0),
+            (x:1.0, y:0.0),
+            (x:1.0, y:1.0),
+            (x:2.0, y:1.0),
+        ];
+        let mid_line = line_string.lines().nth(1).unwrap();
+        let slice_point = mid_line.start + mid_line.delta() * 0.5;
+        assert_eq!(
+            line_string.line_split(0.5),
+            Some(LineSplitResult::FirstSecond(
+                LineString::new(vec![line_string.0[0],line_string.0[1], slice_point]),
+                LineString::new(vec![slice_point, line_string.0[2],line_string.0[3]])
+            ))
+        );
+    }
+    #[test]
+    fn test_measure_linestring_split_first() {
+        let line_string:LineString<f32> = line_string![
+            (x:0.0, y:0.0),
+            (x:1.0, y:0.0),
+        ];
+        assert_eq!(
+            line_string.line_split(1.0),
+            Some(LineSplitResult::First(line_string))
+        );
+    }
+
+    #[test]
+    fn test_measure_linestring_split_second() {
+        let line_string:LineString<f32> = line_string![
+            (x:0.0, y:0.0),
+            (x:1.0, y:0.0),
+        ];
+        assert_eq!(
+            line_string.line_split(0.0),
+            Some(LineSplitResult::Second(line_string))
+        );
+    }
+
 
 }
 
