5a6f5112159b94dd81984d6383d5fa18975cb606
Michael Kirk <michael.code@endoftheworl.de>
2023-10-27 16:01:29 +0200

    fixup! Merge branch 'main' into mkirk/wkt-macro

--- geo/src/algorithm/affine_ops.rs
+++ geo/src/algorithm/affine_ops.rs
@@ -554,7 +554,7 @@ mod tests {
         // test really only needs this, but let's be sure
         assert!(identity.is_identity());
 
-        let mut poly = polygon![(x: 0.0, y: 0.0), (x: 0.0, y: 2.0), (x: 1.0, y: 2.0)];
+        let mut poly = wkt! { POLYGON((0.0 0.0,0.0 2.0,1.0 2.0)) };
         let expected = poly.clone();
         poly.affine_transform_mut(&identity);
         assert_eq!(expected, poly);
