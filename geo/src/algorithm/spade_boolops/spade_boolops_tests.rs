aac98c6467e3d832be5d9889477b0d80ef81c7ba
RobWalt <robwalter96@gmail.com>
2023-10-28 22:44:42 +0200

    chore: implement simple wkt loader fn for tests

--- geo/src/algorithm/spade_boolops/spade_boolops_tests.rs
+++ geo/src/algorithm/spade_boolops/spade_boolops_tests.rs
@@ -1,5 +1,18 @@
 use super::*;
 use geo_types::*;
+use wkt::TryFromWkt;
+
+// helper
+
+fn load_wkt(data_str: &str) -> Result<Vec<Polygon<f32>>, String> {
+    let GeometryCollection(data) =
+        GeometryCollection::<f32>::try_from_wkt_str(data_str).map_err(|e| format!("{e:?}"))?;
+    let data = data
+        .into_iter()
+        .filter_map(|g| g.try_into().ok())
+        .collect::<Vec<_>>();
+    Ok(data)
+}
 
 #[test]
 fn basic_intersection_compiles() {
@@ -9,4 +22,12 @@ fn basic_intersection_compiles() {
     let rect2 = Rect::new(one, one * 3.0);
 
     SpadeBoolops::intersection(&rect1.to_polygon(), &rect2.to_polygon()).unwrap();
+}
+
+#[test]
+fn load_star_works() {
+    _ = pretty_env_logger::try_init();
+    let data = include_str!("./data/star.wkt");
+    let data = load_wkt(data).unwrap();
+    info!("{data:?}");
 }
