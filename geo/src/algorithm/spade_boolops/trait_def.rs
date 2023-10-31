c171ffc8ba3eea31caf3fe88c94fded435864439
RobWalt <robwalter96@gmail.com>
2023-10-31 21:53:40 +0100

    chore: add must_use attributes to boolean operations

--- geo/src/algorithm/spade_boolops/trait_def.rs
+++ geo/src/algorithm/spade_boolops/trait_def.rs
@@ -16,18 +16,21 @@ where
         op_pred: F,
     ) -> SpadeBoolopsResult<T>;
 
+    #[must_use = "Use the difference of these two geometries by binding the result to a variable! (`let result = ...`)"]
     fn difference(p1: &Self, p2: &Self) -> SpadeBoolopsResult<T> {
         Self::boolop(p1, p2, |tri| {
             contains_triangle(p1, tri) && !contains_triangle(p2, tri)
         })
     }
 
+    #[must_use = "Use the intersection of these two geometries by binding the result to a variable! (`let result = ...`)"]
     fn intersection(p1: &Self, p2: &Self) -> SpadeBoolopsResult<T> {
         Self::boolop(p1, p2, |tri| {
             contains_triangle(p1, tri) && contains_triangle(p2, tri)
         })
     }
 
+    #[must_use = "Use the union of these two geometries by binding the result to a variable! (`let result = ...`)"]
     fn union(p1: &Self, p2: &Self) -> SpadeBoolopsResult<T> {
         Self::boolop(p1, p2, |tri| {
             contains_triangle(p1, tri) || contains_triangle(p2, tri)
