b1ae9f91541fa4eaf2d631890a86ea570cc530af
aviac <aviac@mailbox.org>
2024-01-25 21:06:30 +0100

    chore(cleanup): comment benchmark out
    This benchmark needs a public export of the stitch trait which isn't
    given anymore. I added some comments for the after world.
    
    Authored-by: RobWalt <robwalter96@gmail.com>

--- geo/benches/stitch.rs
+++ geo/benches/stitch.rs
@@ -1,17 +1,24 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use geo::stitch::StitchTriangles;
-use geo::TriangulateSpade;
-
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("stitch", |bencher| {
-        let p = geo_test_fixtures::east_baton_rouge::<f32>();
-        let tris = p.unconstrained_triangulation().unwrap();
-
-        bencher.iter(|| {
-            criterion::black_box(criterion::black_box(&tris).stitch_triangulation().unwrap());
-        });
-    });
+// This needs a public export for the stitchtriangles trait. For now we decided to make it private
+// so this benchmark is commented out. In case you need it and the trait still isn't public yet,
+// you need to temporarily change that to make this benchmark work again.
+//
+// use criterion::{criterion_group, criterion_main, criterion};
+// use geo::stitch::StitchTriangles;
+// use geo::TriangulateSpade;
+//
+// fn criterion_benchmark(c: &mut criterion) {
+//     c.bench_function("stitch", |bencher| {
+//         let p = geo_test_fixtures::east_baton_rouge::<f32>();
+//         let tris = p.unconstrained_triangulation().unwrap();
+//
+//         bencher.iter(|| {
+//             criterion::black_box(criterion::black_box(&tris).stitch_triangulation().unwrap());
+//         });
+//     });
+// }
+//
+// criterion_group!(benches, criterion_benchmark);
+// criterion_main!(benches);
+fn main() {
+    println!("Placeholder");
 }
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
