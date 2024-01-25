9e62ccdf0bb782a8262d2671b7ccc97616f556aa
aviac <aviac@mailbox.org>
2024-01-25 18:29:21 +0100

    fix(test): remote pub export
    Authored-by: RobWalt <robwalter96@gmail.com>

--- geo/src/algorithm/mod.rs
+++ geo/src/algorithm/mod.rs
@@ -237,8 +237,7 @@ pub mod simplify_vw;
 pub use simplify_vw::{SimplifyVw, SimplifyVwIdx, SimplifyVwPreserve};
 
 /// Stitch together triangles with adjacent sides. Alternative to unioning triangles via BooleanOps.
-pub mod stitch;
-pub use stitch::StitchTriangles;
+pub(crate) mod stitch;
 
 /// Transform a geometry using PROJ.
 #[cfg(feature = "use-proj")]
