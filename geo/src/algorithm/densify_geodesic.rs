5f72d9572a85b3de54908374ec0f7dbc0850a789
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-08-10 23:21:54 +0400

    feat: implement `DensifyGeodesic` for geo types (T = f64)

--- geo/src/algorithm/densify_geodesic.rs
+++ geo/src/algorithm/densify_geodesic.rs
@@ -1,6 +1,6 @@
 use num_traits::ToPrimitive;
 
-use crate::{CoordFloat, Line, Point};
+use crate::{CoordFloat, CoordsIter, Line, LineString, MultiLineString, MultiPolygon, Point, Polygon, Rect, Triangle};
 
 use crate::{GeodesicIntermediate, GeodesicLength};
 
@@ -39,5 +39,107 @@ fn densify_line(
         let ratio = frac * segment_idx as f64;
         let interpolated_point = Point::from(start).geodesic_intermediate(&Point::from(end), ratio);
         container.push(interpolated_point);
+    }
+}
+
+impl DensifyGeodesic<f64> for MultiPolygon<f64>
+where
+    Line<f64>: GeodesicLength<f64>,
+    LineString<f64>: GeodesicLength<f64>,
+{
+    type Output = MultiPolygon<f64>;
+
+    fn densify_geodesic(&self, max_distance: f64) -> Self::Output {
+        MultiPolygon::new(
+            self.iter().map(|polygon| polygon.densify_geodesic(max_distance)).collect(),
+        )
+    }
+}
+
+impl DensifyGeodesic<f64> for Polygon<f64>
+where
+    Line<f64>: GeodesicLength<f64>,
+    LineString<f64>: GeodesicLength<f64>,
+{
+    type Output = Polygon<f64>;
+
+    fn densify_geodesic(&self, max_distance: f64) -> Self::Output {
+        let densified_exterior = self.exterior().densify_geodesic(max_distance);
+        let densified_interiors = self.interiors().iter().map(|ring| ring.densify_geodesic(max_distance)).collect();
+        Polygon::new(densified_exterior, densified_interiors)
+    }
+}
+
+impl DensifyGeodesic<f64> for MultiLineString<f64>
+where
+    Line<f64>: GeodesicLength<f64>,
+    LineString<f64>: GeodesicLength<f64>,
+{
+    type Output = MultiLineString<f64>;
+
+    fn densify_geodesic(&self, max_distance: f64) -> Self::Output {
+        MultiLineString::new(
+            self.iter().map(|linestring| linestring.densify_geodesic(max_distance)).collect(),
+        )
+    }
+}
+
+impl DensifyGeodesic<f64> for LineString<f64>
+where
+    Line<f64>: GeodesicLength<f64>,
+    LineString<f64>: GeodesicLength<f64>,
+{
+    type Output = LineString<f64>;
+
+    fn densify_geodesic(&self, max_distance: f64) -> Self::Output {
+        if self.coords_count() == 0 {
+            return LineString::new(vec![]);
+        }
+
+        let mut new_line = vec![];
+        self.lines().for_each(|line| densify_line(line, &mut new_line, max_distance));
+        // we're done, push the last coordinate on to finish
+        new_line.push(self.points().last().unwrap());
+        LineString::from(new_line)
+    }
+}
+
+impl DensifyGeodesic<f64> for Line<f64>
+where
+    Line<f64>: GeodesicLength<f64>,
+    LineString<f64>: GeodesicLength<f64>,
+{
+    type Output = LineString<f64>;
+
+    fn densify_geodesic(&self, max_distance: f64) -> Self::Output {
+        let mut new_line = vec![];
+        densify_line(*self, &mut new_line, max_distance);
+        // we're done, push the last coordinate on to finish
+        new_line.push(self.end_point());
+        LineString::from(new_line)
+    }
+}
+
+impl DensifyGeodesic<f64> for Triangle<f64>
+where
+    Line<f64>: GeodesicLength<f64>,
+    LineString<f64>: GeodesicLength<f64>,
+{
+    type Output = Polygon<f64>;
+
+    fn densify_geodesic(&self, max_distance: f64) -> Self::Output {
+        self.to_polygon().densify_geodesic(max_distance)
+    }
+}
+
+impl DensifyGeodesic<f64> for Rect<f64>
+where
+    Line<f64>: GeodesicLength<f64>,
+    LineString<f64>: GeodesicLength<f64>,
+{
+    type Output = Polygon<f64>;
+
+    fn densify_geodesic(&self, max_distance: f64) -> Self::Output {
+        self.to_polygon().densify_geodesic(max_distance)
     }
 }
