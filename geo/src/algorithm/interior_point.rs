a19c5908916b0fdce72d8d632a1b3391ff08ae1e
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-09-28 20:05:54 +0400

    fix: replace `partial_cmp` with `total_cmp` in sorting

--- geo/src/algorithm/interior_point.rs
+++ geo/src/algorithm/interior_point.rs
@@ -210,7 +210,7 @@ fn polygon_interior_point_with_segment_length<T: GeoFloat>(
         let midpoint = Point::new((start.x + end.x) / two, y_mid);
         segments.push((midpoint, length));
     }
-    segments.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap_or(Ordering::Less));
+    segments.sort_by(|a, b| b.1.total_cmp(&a.1));
 
     for (midpoint, segment_length) in segments {
         // some pairs of consecutive points traveling east-west will bound segments inside the
