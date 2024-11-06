1137aef19aca9c2c2b950e0715e2cf4c401dcc11
Stephan Hügel <shugel@tcd.ie>
2024-11-06 19:14:51 +0000

    Add multithreading feature to geo-types top-level docs

--- geo-types/src/lib.rs
+++ geo-types/src/lib.rs
@@ -62,6 +62,8 @@
 //! The following optional [Cargo features] are available:
 //!
 //! - `std`: Enables use of the full `std` library. Enabled by default.
+//! - `multithread`: Enables multi-threaded iteration over `Multi*` geometries. **Disabled**
+//!    by default but **enabled** by `geo`'s default features.
 //! - `approx`: Allows geometry types to be checked for approximate equality with [approx]
 //! - `arbitrary`: Allows geometry types to be created from unstructured input with [arbitrary]
 //! - `serde`: Allows geometry types to be serialized and deserialized with [Serde]
