2c81d3160f07574a5a0885ffda6cbcc484dbd8d4
Kristin Cowalcijk <bo@wherobots.com>
2025-04-24 00:44:23 +0800

    Remove temporary code for exploring the generic design of IntersectsTrait

--- geo-generic-tests/src/wkb/test/trait_test.rs
+++ geo-generic-tests/src/wkb/test/trait_test.rs
@@ -1,78 +1,14 @@
-use geo_generic_alg::*;
 use geo_traits::*;
-use geo_traits_ext::*;
 use geos::WKBWriter;
 
 use crate::wkb::reader::read_wkb;
 
 use super::data::*;
-
-trait IntersectsGeo<Rhs = Self> {
-    fn intersects_geo(&self, rhs: &Rhs) -> bool;
-}
-
-impl<LHS, RHS> IntersectsGeo<RHS> for LHS
-where
-    LHS: GeoTraitExtWithTypeTag,
-    RHS: GeoTraitExtWithTypeTag,
-    LHS: IntersectsTrait<LHS::Tag, RHS::Tag, RHS>,
-{
-    fn intersects_geo(&self, rhs: &RHS) -> bool {
-        self.intersects_trait(rhs)
-    }
-}
-
-trait IntersectsTrait<LM, RM, Rhs = Self> {
-    fn intersects_trait(&self, rhs: &Rhs) -> bool;
-}
-
-impl<T, P, Rhs> IntersectsTrait<PointTag, PointTag, Rhs> for P
-where
-    P: PointTraitExt<T = T>,
-    T: CoordNum,
-    Rhs: PointTraitExt<T = T>,
-{
-    fn intersects_trait(&self, _rhs: &Rhs) -> bool {
-        false
-    }
-}
-
-impl<T, P, Rhs> IntersectsTrait<PointTag, LineStringTag, Rhs> for P
-where
-    P: PointTraitExt<T = T>,
-    T: CoordNum,
-    Rhs: LineStringTraitExt<T = T>,
-{
-    fn intersects_trait(&self, _rhs: &Rhs) -> bool {
-        false
-    }
-}
-
-impl<T, LS, Rhs> IntersectsTrait<LineStringTag, PointTag, Rhs> for LS
-where
-    LS: LineStringTraitExt<T = T>,
-    T: CoordNum,
-    Rhs: PointTraitExt<T = T>,
-{
-    fn intersects_trait(&self, _rhs: &Rhs) -> bool {
-        false
-    }
-}
-
-impl<T, LS, Rhs> IntersectsTrait<LineStringTag, LineStringTag, Rhs> for LS
-where
-    LS: LineStringTraitExt<T = T>,
-    T: CoordNum,
-    Rhs: LineStringTraitExt<T = T>,
-{
-    fn intersects_trait(&self, _rhs: &Rhs) -> bool {
-        false
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use geo_generic_alg::area::AreaTrait;
+    use geo_generic_alg::intersects::Intersects;
+    use geo_generic_alg::*;
 
     use super::*;
 
@@ -88,17 +24,13 @@ mod tests {
 
         match (wkb.as_type(), wkb2.as_type()) {
             (geo_traits::GeometryType::Point(pt), geo_traits::GeometryType::Point(pt2)) => {
-                // let area = pt.area_test();
                 let area = pt.signed_area();
                 assert_eq!(area, 0.0);
                 let area = pt.unsigned_area();
                 assert_eq!(area, 0.0);
 
-                let intersects = pt.intersects_trait(pt2);
-                assert!(!intersects);
-
-                let intersects = pt.intersects_geo(pt2);
-                assert!(!intersects);
+                let intersects = pt.intersects(&pt2);
+                assert!(intersects);
             }
             (geo_traits::GeometryType::Point(pt), geo_traits::GeometryType::LineString(ls)) => {
                 let area = ls.signed_area();
@@ -106,28 +38,19 @@ mod tests {
                 let area = ls.unsigned_area();
                 assert_eq!(area, 0.0);
 
-                let intersects = pt.intersects_trait(ls);
-                assert!(!intersects);
-
-                let intersects = pt.intersects_geo(ls);
-                assert!(!intersects);
+                let intersects = pt.intersects(&ls);
+                assert!(intersects);
             }
             (geo_traits::GeometryType::LineString(ls), geo_traits::GeometryType::Point(pt)) => {
-                let intersects = ls.intersects_trait(pt);
-                assert!(!intersects);
-
-                let intersects = ls.intersects_geo(pt);
-                assert!(!intersects);
+                let intersects = ls.intersects(&pt);
+                assert!(intersects);
             }
             (
                 geo_traits::GeometryType::LineString(ls),
                 geo_traits::GeometryType::LineString(ls2),
             ) => {
-                let intersects = ls.intersects_trait(ls2);
-                assert!(!intersects);
-
-                let intersects = ls.intersects_geo(ls2);
-                assert!(!intersects);
+                let intersects = ls.intersects(&ls2);
+                assert!(intersects);
             }
             _ => panic!("Expected a Point"),
         }
