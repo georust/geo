ab755b9fe1bc7333bb16f404df2b65271c202fc5
cookiedan42 <git@cookiedan42.com>
2025-07-07 00:25:03 +0800

    Faster LineString intersects ~Polygon

--- geo/src/algorithm/intersects/collections.rs
+++ geo/src/algorithm/intersects/collections.rs
@@ -24,9 +24,12 @@ where
 }
 symmetric_intersects_impl!(Coord<T>, Geometry<T>);
 symmetric_intersects_impl!(Line<T>, Geometry<T>);
+symmetric_intersects_impl!(LineString<T>, Geometry<T>);
+symmetric_intersects_impl!(MultiLineString<T>, Geometry<T>);
 symmetric_intersects_impl!(Rect<T>, Geometry<T>);
 symmetric_intersects_impl!(Triangle<T>, Geometry<T>);
 symmetric_intersects_impl!(Polygon<T>, Geometry<T>);
+
 
 impl<T, G> Intersects<G> for GeometryCollection<T>
 where
@@ -43,6 +46,8 @@ where
 }
 symmetric_intersects_impl!(Coord<T>, GeometryCollection<T>);
 symmetric_intersects_impl!(Line<T>, GeometryCollection<T>);
+symmetric_intersects_impl!(LineString<T>, GeometryCollection<T>);
+symmetric_intersects_impl!(MultiLineString<T>, GeometryCollection<T>);
 symmetric_intersects_impl!(Rect<T>, GeometryCollection<T>);
 symmetric_intersects_impl!(Triangle<T>, GeometryCollection<T>);
 symmetric_intersects_impl!(Polygon<T>, GeometryCollection<T>);
