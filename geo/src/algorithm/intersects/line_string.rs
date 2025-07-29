ead3c3c991478fb0acbfc3c93f163aa6420bf09a
cookiedan42 <git@cookiedan42.com>
2025-07-29 18:06:58 +0800

    convert benches into tests

--- geo/src/algorithm/intersects/line_string.rs
+++ geo/src/algorithm/intersects/line_string.rs
@@ -3,7 +3,7 @@ use crate::BoundingRect;
 use crate::*;
 
 // Blanket implementation using self.lines().any().
-macro_rules! blanket_intersects_linestring {
+macro_rules! intersects_line_string_impl {
     ($t:ty) => {
         impl<T> $crate::Intersects<$t> for LineString<T>
         where
@@ -21,12 +21,12 @@ macro_rules! blanket_intersects_linestring {
     };
 }
 
-blanket_intersects_linestring!(Coord<T>);
-blanket_intersects_linestring!(Point<T>);
-blanket_intersects_linestring!(MultiPoint<T>);
+intersects_line_string_impl!(Coord<T>);
+intersects_line_string_impl!(Point<T>);
+intersects_line_string_impl!(MultiPoint<T>);
 
-blanket_intersects_linestring!(Line<T>);
-blanket_intersects_linestring!(LineString<T>);
+intersects_line_string_impl!(Line<T>);
+intersects_line_string_impl!(LineString<T>);
 symmetric_intersects_impl!(LineString<T>, MultiLineString<T>);
 
 impl<T> Intersects<Polygon<T>> for LineString<T>
@@ -113,5 +113,83 @@ where
             return false;
         }
         self.iter().any(|p| p.intersects(rhs))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::wkt;
+
+    #[test]
+    fn test_linestring_inside_polygon() {
+        let ls: LineString<f64> = wkt! {LINESTRING(1 1, 2 2)}.convert();
+        let poly: Polygon<f64> = Rect::new((0, 0), (10, 10)).to_polygon().convert();
+        assert!(ls.intersects(&poly));
+    }
+
+    #[test]
+    fn test_linestring_partial_polygon() {
+        let ls: LineString<f64> = wkt! {LINESTRING(-1 -1, 2 2)}.convert();
+        let poly: Polygon<f64> = Rect::new((0, 0), (10, 10)).to_polygon().convert();
+        assert!(ls.intersects(&poly));
+    }
+    #[test]
+    fn test_linestring_disjoint_polygon() {
+        let ls: LineString<f64> = wkt! {LINESTRING(-1 -1, -2 -2)}.convert();
+        let poly: Polygon<f64> = Rect::new((0, 0), (10, 10)).to_polygon().convert();
+        assert!(!ls.intersects(&poly));
+    }
+    #[test]
+    fn test_linestring_in_polygon_hole() {
+        let ls: LineString<f64> = wkt! {LINESTRING(4 4, 6 6)}.convert();
+        let bound = Rect::new((0, 0), (10, 10)).convert();
+        let hole = Rect::new((1, 1), (9, 9)).convert();
+        let poly = Polygon::new(
+            bound.exterior_coords_iter().collect(),
+            vec![hole.exterior_coords_iter().collect()],
+        );
+
+        assert!(!ls.intersects(&poly));
+    }
+
+    // ls_rect
+    #[test]
+    fn test_linestring_inside_rect() {
+        let ls: LineString<f64> = wkt! {LINESTRING(1 1, 2 2)}.convert();
+        let poly: Rect<f64> = Rect::new((0, 0), (10, 10)).convert();
+        assert!(ls.intersects(&poly));
+    }
+    #[test]
+    fn test_linestring_partial_rect() {
+        let ls: LineString<f64> = wkt! {LINESTRING(-1 -1, 2 2)}.convert();
+        let poly: Rect<f64> = Rect::new((0, 0), (10, 10)).convert();
+        assert!(ls.intersects(&poly));
+    }
+    #[test]
+    fn test_linestring_disjoint_rect() {
+        let ls: LineString<f64> = wkt! {LINESTRING(-1 -1, -2 -2)}.convert();
+        let poly: Rect<f64> = Rect::new((0, 0), (10, 10)).convert();
+        assert!(!ls.intersects(&poly));
+    }
+
+    // ls_triangle
+    #[test]
+    fn test_linestring_inside_triangle() {
+        let ls: LineString<f64> = wkt! {LINESTRING(5 5, 5 4)}.convert();
+        let poly: Triangle<f64> = wkt! {TRIANGLE(0 0, 10 0, 5 10)}.convert();
+        assert!(ls.intersects(&poly));
+    }
+    #[test]
+    fn test_linestring_partial_triangle() {
+        let ls: LineString<f64> = wkt! {LINESTRING(5 5, 5 -4)}.convert();
+        let poly: Triangle<f64> = wkt! {TRIANGLE(0 0, 10 0, 5 10)}.convert();
+        assert!(ls.intersects(&poly));
+    }
+    #[test]
+    fn test_linestring_disjoint_triangle() {
+        let ls: LineString<f64> = wkt! {LINESTRING(5 -5, 5 -4)}.convert();
+        let poly: Triangle<f64> = wkt! {TRIANGLE(0 0, 10 0, 5 10)}.convert();
+        assert!(!ls.intersects(&poly));
     }
 }
