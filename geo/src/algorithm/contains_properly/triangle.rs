e8686481d281ea0e8a8f14e7b7f8515bde1e885f
cookiedan42 <git@cookiedan42.com>
2025-10-03 16:57:44 +0800

    add poly-poly impl

--- geo/src/algorithm/contains_properly/triangle.rs
+++ geo/src/algorithm/contains_properly/triangle.rs
@@ -1,11 +1,11 @@
-use crate::geometry::*;
 use super::{ContainsProperly, impl_contains_properly_from_relate};
-use crate::{GeoFloat};
+use crate::GeoFloat;
+use crate::geometry::*;
 
 impl_contains_properly_from_relate!(Triangle<T>, [
-    Point<T>,MultiPoint<T>, 
-    Line<T>, LineString<T>, MultiLineString<T>,
-    Polygon<T>,MultiPolygon<T>,
-    GeometryCollection<T>,
-    Rect<T>,Triangle<T>
-    ]);
+Point<T>,MultiPoint<T>,
+Line<T>, LineString<T>, MultiLineString<T>,
+Polygon<T>,MultiPolygon<T>,
+GeometryCollection<T>,
+Rect<T>,Triangle<T>
+]);
