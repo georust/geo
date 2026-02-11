ad6c076cadde6d397babbdfeafe024263558f9cc
cookiedan42 <git@cookiedan42.com>
2026-02-11 16:32:01 +0800

    trim scope

--- geo/src/algorithm/intersects/monotone_chain/mod.rs
+++ geo/src/algorithm/intersects/monotone_chain/mod.rs
@@ -76,10 +76,10 @@ mod tests {
         let mcs1: MonotoneChain<f64> = (&ls1).into();
         let mcs2: MonotoneChain<f64> = (&ls2).into();
 
-        assert_eq!(ls0.intersects(&pt), mcs0.intersects(&pt));
-        assert_eq!(ls1.intersects(&pt), ls1.relate(&pt).is_intersects());
-        assert_eq!(ls1.intersects(&pt), mcs1.intersects(&pt));
-        assert_eq!(ls2.intersects(&pt), mcs2.intersects(&pt));
+        assert_eq!(ls0.intersects(&pt.0), mcs0.intersects(&pt.0));
+        assert_eq!(ls1.intersects(&pt.0), ls1.relate(&pt).is_intersects());
+        assert_eq!(ls1.intersects(&pt.0), mcs1.intersects(&pt.0));
+        assert_eq!(ls2.intersects(&pt.0), mcs2.intersects(&pt.0));
     }
 
     #[test]
@@ -106,142 +106,24 @@ mod tests {
         let m_poly: MonotoneChainPolygon<i32> = (&poly).into();
         let m_mpoly: MonotoneChainMultiPolygon<i32> = (&multi_poly).into();
 
-        // forwards
-        let _ = m_ls.intersects(&c);
-        let _ = m_ls.intersects(&pt);
-        let _ = m_ls.intersects(&mpt);
-        let _ = m_ls.intersects(&ln);
-        let _ = m_ls.intersects(&ls);
-        let _ = m_ls.intersects(&multi_ls);
-        let _ = m_ls.intersects(&poly);
-        let _ = m_ls.intersects(&multi_poly);
-        let _ = m_ls.intersects(&rect);
-        let _ = m_ls.intersects(&tri);
-        let _ = m_ls.intersects(&geom);
-        let _ = m_ls.intersects(&gc);
         let _ = m_ls.intersects(&m_ls);
         let _ = m_ls.intersects(&m_mls);
         let _ = m_ls.intersects(&m_poly);
         let _ = m_ls.intersects(&m_mpoly);
 
-        let _ = m_mls.intersects(&c);
-        let _ = m_mls.intersects(&pt);
-        let _ = m_mls.intersects(&mpt);
-        let _ = m_mls.intersects(&ln);
-        let _ = m_mls.intersects(&ls);
-        let _ = m_mls.intersects(&multi_ls);
-        let _ = m_mls.intersects(&poly);
-        let _ = m_mls.intersects(&multi_poly);
-        let _ = m_mls.intersects(&rect);
-        let _ = m_mls.intersects(&tri);
-        let _ = m_mls.intersects(&geom);
-        let _ = m_mls.intersects(&gc);
         let _ = m_mls.intersects(&m_ls);
         let _ = m_mls.intersects(&m_mls);
         let _ = m_mls.intersects(&m_poly);
         let _ = m_mls.intersects(&m_mpoly);
 
-        let _ = m_poly.intersects(&c);
-        let _ = m_poly.intersects(&pt);
-        let _ = m_poly.intersects(&mpt);
-        let _ = m_poly.intersects(&ln);
-        let _ = m_poly.intersects(&ls);
-        let _ = m_poly.intersects(&multi_ls);
-        let _ = m_poly.intersects(&poly);
-        let _ = m_poly.intersects(&multi_poly);
-        let _ = m_poly.intersects(&rect);
-        let _ = m_poly.intersects(&tri);
-        let _ = m_poly.intersects(&geom);
-        let _ = m_poly.intersects(&gc);
         let _ = m_poly.intersects(&m_ls);
         let _ = m_poly.intersects(&m_mls);
         let _ = m_poly.intersects(&m_poly);
         let _ = m_poly.intersects(&m_mpoly);
 
-        let _ = m_mpoly.intersects(&c);
-        let _ = m_mpoly.intersects(&pt);
-        let _ = m_mpoly.intersects(&mpt);
-        let _ = m_mpoly.intersects(&ln);
-        let _ = m_mpoly.intersects(&ls);
-        let _ = m_mpoly.intersects(&multi_ls);
-        let _ = m_mpoly.intersects(&poly);
-        let _ = m_mpoly.intersects(&multi_poly);
-        let _ = m_mpoly.intersects(&rect);
-        let _ = m_mpoly.intersects(&tri);
-        let _ = m_mpoly.intersects(&geom);
-        let _ = m_mpoly.intersects(&gc);
         let _ = m_mpoly.intersects(&m_ls);
         let _ = m_mpoly.intersects(&m_mls);
         let _ = m_mpoly.intersects(&m_poly);
-        let _ = m_mpoly.intersects(&m_mpoly);
-
-        // backwards
-        let _ = c.intersects(&m_ls);
-        let _ = pt.intersects(&m_ls);
-        let _ = mpt.intersects(&m_ls);
-        let _ = ln.intersects(&m_ls);
-        let _ = ls.intersects(&m_ls);
-        let _ = multi_ls.intersects(&m_ls);
-        let _ = poly.intersects(&m_ls);
-        let _ = multi_poly.intersects(&m_ls);
-        let _ = rect.intersects(&m_ls);
-        let _ = tri.intersects(&m_ls);
-        let _ = geom.intersects(&m_ls);
-        let _ = gc.intersects(&m_ls);
-        let _ = m_ls.intersects(&m_ls);
-        let _ = m_mls.intersects(&m_ls);
-        let _ = m_poly.intersects(&m_ls);
-        let _ = m_mpoly.intersects(&m_ls);
-
-        let _ = c.intersects(&m_mls);
-        let _ = pt.intersects(&m_mls);
-        let _ = mpt.intersects(&m_mls);
-        let _ = ln.intersects(&m_mls);
-        let _ = ls.intersects(&m_mls);
-        let _ = multi_ls.intersects(&m_mls);
-        let _ = poly.intersects(&m_mls);
-        let _ = multi_poly.intersects(&m_mls);
-        let _ = rect.intersects(&m_mls);
-        let _ = tri.intersects(&m_mls);
-        let _ = geom.intersects(&m_mls);
-        let _ = gc.intersects(&m_mls);
-        let _ = m_ls.intersects(&m_mls);
-        let _ = m_mls.intersects(&m_mls);
-        let _ = m_poly.intersects(&m_mls);
-        let _ = m_mpoly.intersects(&m_mls);
-
-        let _ = c.intersects(&m_poly);
-        let _ = pt.intersects(&m_poly);
-        let _ = mpt.intersects(&m_poly);
-        let _ = ln.intersects(&m_poly);
-        let _ = ls.intersects(&m_poly);
-        let _ = multi_ls.intersects(&m_poly);
-        let _ = poly.intersects(&m_poly);
-        let _ = multi_poly.intersects(&m_poly);
-        let _ = rect.intersects(&m_poly);
-        let _ = tri.intersects(&m_poly);
-        let _ = geom.intersects(&m_poly);
-        let _ = gc.intersects(&m_poly);
-        let _ = m_ls.intersects(&m_poly);
-        let _ = m_mls.intersects(&m_poly);
-        let _ = m_poly.intersects(&m_poly);
-        let _ = m_mpoly.intersects(&m_poly);
-
-        let _ = c.intersects(&m_mpoly);
-        let _ = pt.intersects(&m_mpoly);
-        let _ = mpt.intersects(&m_mpoly);
-        let _ = ln.intersects(&m_mpoly);
-        let _ = ls.intersects(&m_mpoly);
-        let _ = multi_ls.intersects(&m_mpoly);
-        let _ = poly.intersects(&m_mpoly);
-        let _ = multi_poly.intersects(&m_mpoly);
-        let _ = rect.intersects(&m_mpoly);
-        let _ = tri.intersects(&m_mpoly);
-        let _ = geom.intersects(&m_mpoly);
-        let _ = gc.intersects(&m_mpoly);
-        let _ = m_ls.intersects(&m_mpoly);
-        let _ = m_mls.intersects(&m_mpoly);
-        let _ = m_poly.intersects(&m_mpoly);
         let _ = m_mpoly.intersects(&m_mpoly);
     }
 }
--- geo/src/algorithm/intersects/monotone_chain/monotone_chain.rs
+++ geo/src/algorithm/intersects/monotone_chain/monotone_chain.rs
@@ -3,14 +3,52 @@ use crate::intersects::has_disjoint_bboxes;
 use crate::monotone_chain::{MonotoneChain, MonotoneChainSegment};
 use crate::{BoundingRect, GeoNum, Intersects};
 
-//TODO: check if order of montone chains vs monotone chain segments affects performance
-
-impl<'a, G, T: GeoNum> Intersects<G> for MonotoneChain<'a, T>
+impl<'a, T> Intersects<Coord<T>> for MonotoneChain<'a, T>
 where
-    G: BoundingRect<T>,
-    MonotoneChainSegment<'a, T>: Intersects<G>,
+    T: GeoNum,
 {
-    fn intersects(&self, rhs: &G) -> bool {
+    fn intersects(&self, rhs: &Coord<T>) -> bool {
+        if has_disjoint_bboxes(self, rhs) {
+            return false;
+        }
+
+        self.segment_iter().any(|seg| seg.intersects(rhs))
+    }
+}
+symmetric_intersects_impl!(Coord<T>, MonotoneChain<'a, T>);
+
+impl<'a, T> Intersects<Line<T>> for MonotoneChain<'a, T>
+where
+    T: GeoNum,
+{
+    fn intersects(&self, rhs: &Line<T>) -> bool {
+        if has_disjoint_bboxes(self, rhs) {
+            return false;
+        }
+
+        self.segment_iter().any(|seg| seg.intersects(rhs))
+    }
+}
+symmetric_intersects_impl!(Line<T>, MonotoneChain<'a, T>);
+
+impl<'a, T> Intersects<MonotoneChain<'a, T>> for MonotoneChain<'a, T>
+where
+    T: GeoNum,
+{
+    fn intersects(&self, rhs: &MonotoneChain<'a, T>) -> bool {
+        if has_disjoint_bboxes(self, rhs) {
+            return false;
+        }
+
+        self.segment_iter().any(|seg| seg.intersects(rhs))
+    }
+}
+
+impl<'a, T> Intersects<MonotoneChainSegment<'a, T>> for MonotoneChain<'a, T>
+where
+    T: GeoNum,
+{
+    fn intersects(&self, rhs: &MonotoneChainSegment<'a, T>) -> bool {
         if has_disjoint_bboxes(self, rhs) {
             return false;
         }
@@ -20,20 +58,3 @@ where
 }
 
 symmetric_intersects_impl!(MonotoneChainSegment<'a, T>, MonotoneChain<'a, T>);
-
-// commented out if they are implemented by blanket impl in main `Intersects` trait
-symmetric_intersects_impl!(Coord<T>, MonotoneChain<'a, T>);
-// symmetric_intersects_impl!(Point<T>,MonotoneChain<'a, T>);
-// symmetric_intersects_impl!(MultiPoint<T>,MonotoneChain<'a, T>);
-
-symmetric_intersects_impl!(Line<T>, MonotoneChain<'a, T>);
-symmetric_intersects_impl!(LineString<T>, MonotoneChain<'a, T>);
-// symmetric_intersects_impl!(MultiLineString<T>,MonotoneChain<'a, T>);
-
-symmetric_intersects_impl!(Polygon<T>, MonotoneChain<'a, T>);
-// symmetric_intersects_impl!(MultiPolygon<T>,MonotoneChain<'a, T>);
-symmetric_intersects_impl!(Rect<T>, MonotoneChain<'a, T>);
-symmetric_intersects_impl!(Triangle<T>, MonotoneChain<'a, T>);
-
-// symmetric_intersects_impl!(Geometry<T>,MonotoneChain<'a, T>);
-// symmetric_intersects_impl!(GeometryCollection<T>,MonotoneChain<'a, T>);
